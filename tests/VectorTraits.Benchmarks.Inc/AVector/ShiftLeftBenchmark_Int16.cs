a92fd68b5df631718e31ea010539b6243dc7ce1e
zhouyuelin <zyl910@sina.com>
2022-11-21 22:45:52 +0800

    Add ShiftLeftBenchmark_Int16

---
+++ tests/VectorTraits.Benchmarks.Inc/AVector/ShiftLeftBenchmark_Int16.cs
@@ -0,0 +1,68 @@
+﻿using BenchmarkDotNet.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Numerics;
+using System.Runtime.CompilerServices;
+#if NETCOREAPP3_0_OR_GREATER
+using System.Runtime.Intrinsics;
+using System.Runtime.Intrinsics.X86;
+#endif
+#if NET5_0_OR_GREATER
+using System.Runtime.Intrinsics.Arm;
+#endif // NET5_0_OR_GREATER
+using System.Text;
+using Zyl.VectorTraits.Benchmarks;
+
+namespace VectorTraits.Benchmarks.AVector {
+    //using BenchmarkAttribute = FakeBenchmarkAttribute;
+
+    // My type.
+    using TMy = Int16;
+
+    /// <summary>
+    /// Shift left benchmark - Int16.
+    /// </summary>
+    public partial class ShiftLeftBenchmark_Int16 : AbstractSharedBenchmark {
+        // -- TMy ref --
+        private static ref TMy dstTMy => ref dstInt16;
+        private static ref TMy baselineTMy => ref baselineInt16;
+        private static TMy[] srcArray => srcArrayInt16;
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private void CheckResult(string name) {
+            CheckResultInt16(name);
+        }
+
+
+
+        // -- var --
+        private const byte DefaultShiftCount = 4;
+
+        /// <summary>
+        /// Sum shift left logical for int array - base.
+        /// </summary>
+        /// <param name="src">Source array.</param>
+        /// <param name="srcCount">Source count</param>
+        /// <param name="shiftCount">Shift count.</param>
+        /// <returns>Returns the sum.</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static TMy StaticSumSLLScalar(TMy[] src, int srcCount, int shiftCount) {
+            TMy rt = 0; // Result.
+            for (int i = 0; i < srcCount; ++i) {
+                rt += (TMy)(src[i] << shiftCount);
+            }
+            return rt;
+        }
+
+        [Benchmark(Baseline = true)]
+        public void SumSLLScalar() {
+            dstTMy = StaticSumSLLScalar(srcArray, srcArray.Length, DefaultShiftCount);
+            if (CheckMode) {
+                baselineTMy = dstTMy;
+                BenchmarkUtil.WriteLine(string.Format("# StaticSumSLLScalar:\t{0}", baselineTMy));
+            }
+        }
+
+    }
+}
--- tests/VectorTraits.Benchmarks/Program.cs
+++ tests/VectorTraits.Benchmarks/Program.cs
@@ -1,13 +1,29 @@
-﻿using System;
+﻿using BenchmarkDotNet.Running;
+using System;
 using System.IO;
+using VectorTraits.Benchmarks.AVector;
 
 namespace Zyl.VectorTraits.Benchmarks {
     class Program {
         static void Main(string[] args) {
-            TextWriter writer = Console.Out;
-            writer.WriteLine("VectorTraits.Benchmarks");
-            writer.WriteLine();
-            TraitsOutput.OutputEnvironment(writer);
+            bool useBenchmark = false;
+            if (args.Length>=1) {
+                bool.TryParse(args[0], out useBenchmark);
+            }
+            // run.
+            if (useBenchmark) {
+                //Summary summary = BenchmarkRunner.Run<ShiftLeftBenchmark_Int16>();
+                var summary = BenchmarkRunner.Run(typeof(ShiftLeftBenchmark_Int16).Assembly);
+            } else {
+                string indent = "";
+                TextWriter writer = Console.Out;
+                writer.WriteLine("VectorTraits.Benchmarks");
+                writer.WriteLine();
+                BenchmarkUtil.OutputEnvironment(writer, indent);
+                writer.WriteLine();
+                BenchmarkMain.RunBenchmark(writer, indent);
+            }
+            //Console.ReadLine();
         }
     }
 }
