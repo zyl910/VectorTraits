73487b79bca9f62ee5034cbf713fd398fb4ee986
zhouyuelin <zyl910@sina.com>
2022-11-21 20:17:00 +0800

    Add VectorTraits.Benchmarks.csproj

---
+++ tests/VectorTraits.Benchmarks/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.IO;
+
+namespace VectorTraits.Benchmarks {
+    class Program {
+        static void Main(string[] args) {
+            TextWriter writer = Console.Out;
+            writer.WriteLine("VectorTraits.Benchmarks");
+            writer.WriteLine();
+            TraitsOutput.OutputEnvironment(writer);
+        }
+    }
+}
