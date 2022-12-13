74d25383591948533fdde5babd7a7edf541e98a8
zhouyuelin <zyl910@sina.com>
2022-12-14 00:55:14 +0800

    VectorTraits.Benchmarks Program add AloneTest

--- tests/VectorTraits.Benchmarks/Program.cs
+++ tests/VectorTraits.Benchmarks/Program.cs
@@ -5,11 +5,45 @@ using BenchmarkDotNet.Running;
 using BenchmarkDotNet.Toolchains.InProcess.Emit;
 using System;
 using System.IO;
+using System.Numerics;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using Zyl.VectorTraits.Benchmarks.AVector.S;
 
 namespace Zyl.VectorTraits.Benchmarks {
     class Program {
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        static void AloneTest(TextWriter writer) {
+            // set DOTNET_JitDisasm=AloneTest
+            // export DOTNET_JitDisasm=AloneTest
+            // dotnet VectorTraits.Benchmarks.dll 3 >1.txt
+
+            // -- body --
+            Vector<long> src = ~Vector<long>.Zero;
+            int shiftAmount = 1;
+            try {
+                var dst = Vectors.ShiftLeft(src, shiftAmount);
+                writer.WriteLine($"ShiftLeftLogical:\tOK. {dst}");
+            } catch (Exception ex) {
+                writer.WriteLine($"ShiftLeftLogical:\tFail!. {ex}");
+            }
+            try {
+                var dst = Vectors.ShiftRightArithmetic(src, shiftAmount);
+                writer.WriteLine($"ShiftRightArithmetic:\tOK. {dst}");
+            } catch (Exception ex) {
+                writer.WriteLine($"ShiftRightArithmetic:\tFail!. {ex}");
+            }
+            try {
+                var dst = Vectors.ShiftRightLogical(src, shiftAmount);
+                writer.WriteLine($"ShiftRightLogical:\tOK. {dst}");
+            } catch (Exception ex) {
+                writer.WriteLine($"ShiftRightLogical:\tFail!. {ex}");
+            }
+            var o = new IntroDisassemblyDry();
+            int n = o.RunVector128();
+            writer.WriteLine("RunVector128:\t{0}", n);
+        }
+
         static void Main(string[] args) {
             TextWriter writer = Console.Out;
             writer.WriteLine("VectorTraits.Benchmarks");
@@ -17,7 +51,7 @@ namespace Zyl.VectorTraits.Benchmarks {
             // 0: Benchmark all with my BenchmarkMain.
             // 1: Benchmark all with BenchmarkDotNet.
             // 2: Benchmark item with BenchmarkDotNet.
-            // 3: Running special method (RunVector128).
+            // 3: Running special method (AloneTest).
             int benchmarkMode = 0;
             if (args.Length >= 1) {
                 if (!int.TryParse(args[0], out benchmarkMode)) {
@@ -26,9 +60,7 @@ namespace Zyl.VectorTraits.Benchmarks {
             }
             writer.WriteLine("benchmarkMode:\t{0}", benchmarkMode);
             if (benchmarkMode == 3) {
-                var o = new IntroDisassemblyDry();
-                int n = o.RunVector128();
-                writer.WriteLine("RunVector128:\t{0}", n);
+                AloneTest(writer);
             } else if (benchmarkMode > 0) {
                 Architecture architecture = RuntimeInformation.OSArchitecture;
                 var config = DefaultConfig.Instance;
@@ -55,6 +87,8 @@ namespace Zyl.VectorTraits.Benchmarks {
                 TraitsOutput.ParseWaitDebugAndReadKey(args);
                 writer.WriteLine();
                 BenchmarkMain.RunBenchmark(writer, indent);
+                writer.WriteLine();
+                AloneTest(writer);
             }
             //Console.ReadLine();
         }
