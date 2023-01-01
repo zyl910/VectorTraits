76c6d000efe4ccf05fcfa332b7bb8d25f178c349
zhouyuelin <zyl910@sina.com>
2023-01-01 17:42:15 +0800

    Instructions for updating ShuffleControlG4

--- src/VectorTraits/ShuffleControlG4.cs
+++ src/VectorTraits/ShuffleControlG4.cs
@@ -8,8 +8,10 @@ namespace Zyl.VectorTraits {
     /// Shuffle control constants for "4-element group" ("4元素组"的换位控制常数) .
     /// </summary>
     /// <remarks>
-    /// <para>Constant names are referenced to HLSL(High-level shader language)/GLSL(OpenGL Shading Language), using the Little-Endian mode (常量名参考了 HLSL(高级着色语言)/GLSL(OpenGL着色语言), 使用的是小端模式). </para>
+    /// <para>Constant names are referenced to HLSL(High-level shader language)/GLSL(OpenGL Shading Language), using the Little-Endian mode. It is like GetByteByBit2R, and the similarly inverse-ordered _MM_SHUFFLE (常量名参考了 HLSL(高级着色语言)/GLSL(OpenGL着色语言), 使用的是小端模式. 它像 GetByteByBit2R, 以及类似逆序的 _MM_SHUFFLE). </para>
     /// </remarks>
+    /// <seealso cref="BitUtil.GetByteByBit2R(byte, byte, byte, byte)"/>
+    /// <seealso cref="BitUtil._MM_SHUFFLE(byte, byte, byte, byte)"/>
     public static class ShuffleControlG4 {
         /// <summary>XXXX := GetByteByBit2R(0, 0, 0, 0) = _MM_SHUFFLE(0, 0, 0, 0) = (0 &lt;&lt; 6) | (0 &lt;&lt; 4) | (0 &lt;&lt; 2) | 0 = 0x00 = 0 .</summary>
         public const byte XXXX = 0x00;
