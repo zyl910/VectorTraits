a46f01181308850201ad24d2cdab3442ba2a6051
zhouyuelin <zyl910@sina.com>
2023-01-02 20:54:49 +0800

    Add Vector128X3 ~ Vector128X8 .

---
+++ src/VectorTraits/Tuples/Vector128X8.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Text;
+#if NETCOREAPP3_0_OR_GREATER
+using System.Runtime.Intrinsics;
+
+namespace Zyl.VectorTraits.Tuples {
+    /// <summary>
+    /// Represents a <see cref="Vector128"/> tuple with 8 components (表示具有8个组件的向量128元组).
+    /// </summary>
+    /// <typeparam name="T">The vector element type (向量中的元素的类型).</typeparam>
+#if NETCOREAPP3_0_OR_GREATER
+#else
+    [Obsolete("Need .NET Core 3.0+ / .NET 5.0+")]
+#endif // NETCOREAPP3_0_OR_GREATER
+#if NET20_OR_GREATER || NETSTANDARD2_0_OR_GREATER || NETCOREAPP1_0_OR_GREATER
+    [Serializable]
+#endif
+    [StructLayout(LayoutKind.Auto)]
+    public struct Vector128X8<T> : IEquatable<Vector128X8<T>>, IStructuralEquatable, IStructuralComparable, IComparable, IComparable<Vector128X8<T>>
+#if NET471_OR_GREATER || NETSTANDARD2_1_OR_GREATER || NETCOREAPP2_0_OR_GREATER
+            , ITuple
+#endif
+#if NETX_0_OR_GREATER
+            , IFormattable
+#endif
+            where T : struct {
+        /// <summary>Value.</summary>
+#if NETCOREAPP3_0_OR_GREATER
+        public (Vector128<T>, Vector128<T>, Vector128<T>, Vector128<T>, Vector128<T>, Vector128<T>, Vector128<T>, Vector128<T>) Val;
+
+        /// <summary>
+        /// Create Vector128X8 .
+        /// </summary>
+        /// <param name="val">Source value.</param>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public Vector128X8((Vector128<T>, Vector128<T>, Vector128<T>, Vector128<T>, Vector128<T>, Vector128<T>, Vector128<T>, Vector128<T>) val) {
+            Val = val;
+        }
+#else
+        public (byte, byte, byte, byte, byte, byte, byte, byte) Val;
+#endif // NETCOREAPP3_0_OR_GREATER
+
+#if NET471_OR_GREATER || NETSTANDARD2_1_OR_GREATER || NETCOREAPP2_0_OR_GREATER
+        /// <inheritdoc/>
+        readonly int ITuple.Length => ((ITuple)Val).Length;
+
+        /// <inheritdoc/>
+        readonly object? ITuple.this[int index] => ((ITuple)Val)[index];
+#endif
+
+        /// <inheritdoc/>
+        public readonly int CompareTo(Vector128X8<T> other) {
+            return Val.CompareTo(other.Val);
+        }
+
+        /// <inheritdoc/>
+        readonly int IStructuralComparable.CompareTo(object? other, IComparer comparer) {
+            return ((IStructuralComparable)Val).CompareTo(other, comparer);
+        }
+
+        /// <inheritdoc/>
+        readonly int IComparable.CompareTo(object? obj) {
+            return ((IComparable)Val).CompareTo(obj);
+        }
+
+        /// <inheritdoc/>
+        public override readonly bool Equals(object? obj) {
+            return obj is Vector128X8<T> x &&
+                   Val.Equals(x.Val);
+        }
+
+        /// <inheritdoc/>
+        public readonly bool Equals(Vector128X8<T> other) {
+            return Val.Equals(other.Val);
+        }
+
+        /// <inheritdoc/>
+        readonly bool IStructuralEquatable.Equals(object? other, IEqualityComparer comparer) {
+            return ((IStructuralEquatable)Val).Equals(other, comparer);
+        }
+
+        /// <inheritdoc/>
+        public override readonly int GetHashCode() {
+            return Val.GetHashCode();
+        }
+
+        /// <inheritdoc/>
+        readonly int IStructuralEquatable.GetHashCode(IEqualityComparer comparer) {
+            return ((IStructuralEquatable)Val).GetHashCode(comparer);
+        }
+
+        /// <inheritdoc/>
+        public override readonly string? ToString() {
+            return Val.ToString();
+        }
+
+#if NETX_0_OR_GREATER
+        /// <inheritdoc/>
+        public readonly string ToString(string? format, IFormatProvider? formatProvider) {
+            string rt = Val.Item1.ToString(format, formatProvider)
+                + ", " + Val.Item2.ToString(format, formatProvider)
+                + ", " + Val.Item3.ToString(format, formatProvider)
+                + ", " + Val.Item4.ToString(format, formatProvider)
+                + ", " + Val.Item5.ToString(format, formatProvider)
+                + ", " + Val.Item6.ToString(format, formatProvider)
+                + ", " + Val.Item7.ToString(format, formatProvider)
+                + ", " + Val.Rest.Item1.ToString(format, formatProvider)
+            ;
+            return "(" + rt + ")";
+        }
+#endif
+
+#if NETCOREAPP3_0_OR_GREATER
+        /// <summary>
+        /// An implicit operator to convert a Vector128X8 value to a ValueTuple.
+        /// </summary>
+        /// <param name="src">Source value.</param>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static implicit operator (Vector128<T>, Vector128<T>, Vector128<T>, Vector128<T>, Vector128<T>, Vector128<T>, Vector128<T>, Vector128<T>)(Vector128X8<T> src) {
+            return src.Val;
+        }
+
+        /// <summary>
+        /// An explicit operator to convert a ValueTuple value to a Vector128X8.
+        /// </summary>
+        /// <param name="src">Source value.</param>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static explicit operator Vector128X8<T>((Vector128<T>, Vector128<T>, Vector128<T>, Vector128<T>, Vector128<T>, Vector128<T>, Vector128<T>, Vector128<T>) src) {
+            return new Vector128X8<T>(src);
+        }
+#endif // NETCOREAPP3_0_OR_GREATER
+
+        /// <inheritdoc/>
+        public static bool operator ==(Vector128X8<T> left, Vector128X8<T> right) {
+            return left.Equals(right);
+        }
+
+        /// <inheritdoc/>
+        public static bool operator !=(Vector128X8<T> left, Vector128X8<T> right) {
+            return !(left == right);
+        }
+    }
+}
+#endif // NETCOREAPP3_0_OR_GREATER
--- tests/VectorTraits.Tests/Tuples/Vector128XTupleTests.cs
+++ tests/VectorTraits.Tests/Tuples/Vector128XTupleTests.cs
@@ -35,6 +35,29 @@ namespace Zyl.VectorTraits.Tests.Tuples {
             }
         }
 
+        [TestCase((float)1)]
+        [TestCase((double)2)]
+        [TestCase((sbyte)3)]
+        [TestCase((byte)4)]
+        [TestCase((short)5)]
+        [TestCase((ushort)6)]
+        [TestCase((int)7)]
+        [TestCase((uint)8)]
+        [TestCase((long)9)]
+        [TestCase((ulong)10)]
+        public void AsByteX8Test<T>(T src) where T : struct {
+            (Vector128<T>, Vector128<T>, Vector128<T>, Vector128<T>, Vector128<T>, Vector128<T>, Vector128<T>, Vector128<T>) tuple =
+                (Vector128s<T>.Demo, Vector128s.Create<T>(src), Vector128s<T>.V3, Vector128s<T>.V4, Vector128s<T>.V5, Vector128s<T>.V6, Vector128s<T>.V7, Vector128s<T>.V8);
+            Vector128X8<T> vx = Vector128XTuple.Create(tuple);
+            Console.WriteLine($"Vector128XTuple.Create:\t{vx}");
+            //Vector128X8<byte> vxByte = vx.AsByte();
+            //Console.WriteLine($"AsByte:\t{vxByte}");
+            //if (Vector<T>.Count == Vector128<T>.Count) {
+            //    VectorX8<byte> vScalable = vxByte.AsVector();
+            //    Console.WriteLine($"AsVector:\t{vScalable}");
+            //}
+        }
+
 #endif // NETCOREAPP3_0_OR_GREATER
     }
 }
