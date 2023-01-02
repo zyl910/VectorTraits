70d96d155020bc2a5fa762293d5b89d0694ac3aa
zhouyuelin <zyl910@sina.com>
2023-01-02 16:07:33 +0800

    Add Vector128XTuple, Vector128XTupleTests .

---
+++ src/VectorTraits/Tuples/Vector128XTuple.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Runtime.CompilerServices;
+using System.Text;
+#if NETCOREAPP3_0_OR_GREATER
+using System.Runtime.Intrinsics;
+
+namespace Zyl.VectorTraits.Tuples {
+    /// <summary>
+    /// Provides static methods for creating <see cref="Vector128"/> tuples (提供用于创建向量128元组的静态方法).
+    /// </summary>
+    public static partial class Vector128XTuple {
+
+        /// <summary>
+        /// Creates a new vector tuple with 2 components (创建含有 2 个组件的新向量元组).
+        /// </summary>
+        /// <typeparam name="T">The vector element type (向量中的元素的类型).</typeparam>
+        /// <param name="item1">The value of the 1st component (第1个组件的值).</param>
+        /// <param name="item2">The value of the 2nd component (第2个组件的值).</param>
+        /// <returns>A vector tuple with 2 components (具有2个组件的向量元组).</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static Vector128X2<T> Create<T>(Vector128<T> item1, Vector128<T> item2) where T : struct {
+            return new Vector128X2<T>((item1, item2));
+        }
+
+        /// <summary>
+        /// Creates a new vector tuple with 2 components by ValueTuple (根据值元组, 创建含有 2 个组件的新向量元组).
+        /// </summary>
+        /// <typeparam name="T">The vector element type (向量中的元素的类型).</typeparam>
+        /// <param name="val">The value (值).</param>
+        /// <returns>A vector tuple with 2 components (具有2个组件的向量元组).</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static Vector128X2<T> Create<T>((Vector128<T>, Vector128<T>) val) where T : struct {
+            return new Vector128X2<T>(val);
+        }
+
+
+        /// <summary>Reinterprets a <see cref="Vector128X2{T}" /> as a new <see cref="VectorX2{T}" />.</summary>
+        /// <typeparam name="T">The type of the vectors.</typeparam>
+        /// <param name="value">The vector to reinterpret.</param>
+        /// <returns><paramref name="value" /> reinterpreted as a new <see cref="VectorX2{T}" />.</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static VectorX2<T> AsVector<T>(this Vector128X2<T> value) where T : struct {
+            return Unsafe.As<Vector128X2<T>, VectorX2<T>>(ref value);
+        }
+
+        /// <summary>Reinterprets a <see cref="VectorX2{T}" /> as a new <see cref="Vector128X2{T}" />.</summary>
+        /// <typeparam name="T">The type of the vectors.</typeparam>
+        /// <param name="value">The vector to reinterpret.</param>
+        /// <returns><paramref name="value" /> reinterpreted as a new <see cref="Vector128X2{T}" />.</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static Vector128X2<T> AsVector128<T>(this VectorX2<T> value) where T : struct {
+            return Unsafe.As<VectorX2<T>, Vector128X2<T>>(ref value);
+        }
+
+        /// <summary>Reinterprets a <see cref="Vector128X2{TFrom}" /> as a new <see cref="Vector128X2{TTo}" />.</summary>
+        /// <typeparam name="TFrom">The type of the input vector.</typeparam>
+        /// <typeparam name="TTo">The type of the vector <paramref name="vector" /> should be reinterpreted as.</typeparam>
+        /// <param name="src">The source to reinterpret.</param>
+        /// <returns><paramref name="src" /> reinterpreted as a new <see cref="Vector128X2{TTo}" />.</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static Vector128X2<TTo> As<TFrom, TTo>(this Vector128X2<TFrom> src)
+            where TFrom : struct
+            where TTo : struct {
+            return Unsafe.As<Vector128X2<TFrom>, Vector128X2<TTo>>(ref src);
+        }
+
+        /// <summary>Reinterprets the bits of a specified vector into those of a vector of unsigned bytes.</summary>
+        /// <param name="value">The source vector.</param>
+        /// <typeparam name="T">The vector type. <typeparamref name="T" /> can be any primitive numeric type.</typeparam>
+        /// <returns>The reinterpreted vector.</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static Vector128X2<byte> AsByte<T>(this Vector128X2<T> value) where T : struct {
+            return value.As<T, byte>();
+        }
+
+        /// <summary>Reinterprets the bits of a specified vector into those of a vector of signed bytes.</summary>
+        /// <param name="value">The source vector.</param>
+        /// <typeparam name="T">The vector type. <typeparamref name="T" /> can be any primitive numeric type.</typeparam>
+        /// <returns>The reinterpreted vector.</returns>
+        [CLSCompliant(false)]
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static Vector128X2<sbyte> AsSByte<T>(this Vector128X2<T> value) where T : struct {
+            return value.As<T, sbyte>();
+        }
+
+        /// <summary>Reinterprets the bits of a specified vector into those of a vector of unsigned 16-bit integers.</summary>
+        /// <param name="value">The source vector.</param>
+        /// <typeparam name="T">The vector type. <typeparamref name="T" /> can be any primitive numeric type.</typeparam>
+        /// <returns>The reinterpreted vector.</returns>
+        [CLSCompliant(false)]
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static Vector128X2<ushort> AsUInt16<T>(this Vector128X2<T> value) where T : struct {
+            return value.As<T, ushort>();
+        }
+
+        /// <summary>Reinterprets the bits of a specified vector into those of a vector of 16-bit integers.</summary>
+        /// <param name="value">The source vector.</param>
+        /// <typeparam name="T">The vector type. <typeparamref name="T" /> can be any primitive numeric type.</typeparam>
+        /// <returns>The reinterpreted vector.</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static Vector128X2<short> AsInt16<T>(this Vector128X2<T> value) where T : struct {
+            return value.As<T, short>();
+        }
+
+        /// <summary>Reinterprets the bits of a specified vector into those of a vector of unsigned integers.</summary>
+        /// <param name="value">The source vector.</param>
+        /// <typeparam name="T">The vector type. <typeparamref name="T" /> can be any primitive numeric type.</typeparam>
+        /// <returns>The reinterpreted vector.</returns>
+        [CLSCompliant(false)]
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static Vector128X2<uint> AsUInt32<T>(this Vector128X2<T> value) where T : struct {
+            return value.As<T, uint>();
+        }
+
+        /// <summary>Reinterprets the bits of a specified vector into those of a vector of integers.</summary>
+        /// <param name="value">The source vector.</param>
+        /// <typeparam name="T">The vector type. <typeparamref name="T" /> can be any primitive numeric type.</typeparam>
+        /// <returns>The reinterpreted vector.</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static Vector128X2<int> AsInt32<T>(this Vector128X2<T> value) where T : struct {
+            return value.As<T, int>();
+        }
+
+        /// <summary>Reinterprets the bits of a specified vector into those of a vector of unsigned long integers.</summary>
+        /// <param name="value">The source vector.</param>
+        /// <typeparam name="T">The vector type. <typeparamref name="T" /> can be any primitive numeric type.</typeparam>
+        /// <returns>The reinterpreted vector.</returns>
+        [CLSCompliant(false)]
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static Vector128X2<ulong> AsUInt64<T>(this Vector128X2<T> value) where T : struct {
+            return value.As<T, ulong>();
+        }
+
+        /// <summary>Reinterprets the bits of a specified vector into those of a vector of long integers.</summary>
+        /// <param name="value">The source vector.</param>
+        /// <typeparam name="T">The vector type. <typeparamref name="T" /> can be any primitive numeric type.</typeparam>
+        /// <returns>The reinterpreted vector.</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static Vector128X2<long> AsInt64<T>(this Vector128X2<T> value) where T : struct {
+            return value.As<T, long>();
+        }
+
+        /// <summary>Reinterprets the bits of a specified vector into those of a single-precision floating-point vector.</summary>
+        /// <param name="value">The source vector.</param>
+        /// <typeparam name="T">The vector type. <typeparamref name="T" /> can be any primitive numeric type.</typeparam>
+        /// <returns>The reinterpreted vector.</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static Vector128X2<float> AsSingle<T>(this Vector128X2<T> value) where T : struct {
+            return value.As<T, float>();
+        }
+
+        /// <summary>Reinterprets the bits of a specified vector into those of a double-precision floating-point vector.</summary>
+        /// <param name="value">The source vector.</param>
+        /// <typeparam name="T">The vector type. <typeparamref name="T" /> can be any primitive numeric type.</typeparam>
+        /// <returns>The reinterpreted vector.</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static Vector128X2<double> AsDouble<T>(this Vector128X2<T> value) where T : struct {
+            return value.As<T, double>();
+        }
+
+        /// <summary>Reinterprets the bits of a specified vector into those of a vector of native integers.</summary>
+        /// <param name="value">The source vector.</param>
+        /// <typeparam name="T">The vector type. <typeparamref name="T" /> can be any primitive numeric type.</typeparam>
+        /// <returns>The reinterpreted vector.</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static Vector128X2<nint> AsNInt<T>(this Vector128X2<T> value) where T : struct {
+            return value.As<T, nint>();
+        }
+
+        /// <summary>Reinterprets the bits of a specified vector into those of a vector of unsigned native integers.</summary>
+        /// <param name="value">The source vector.</param>
+        /// <typeparam name="T">The vector type. <typeparamref name="T" /> can be any primitive numeric type.</typeparam>
+        /// <returns>The reinterpreted vector.</returns>
+        [CLSCompliant(false)]
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static Vector128X2<nuint> AsNUInt<T>(this Vector128X2<T> value) where T : struct {
+            return value.As<T, nuint>();
+        }
+
+    }
+}
+#endif // NETCOREAPP3_0_OR_GREATER
---
+++ tests/VectorTraits.Tests/Tuples/Vector128XTupleTests.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Numerics;
+#if NETCOREAPP3_0_OR_GREATER
+using System.Runtime.Intrinsics;
+#endif // NETCOREAPP3_0_OR_GREATER
+using Zyl.VectorTraits.Tuples;
+
+namespace Zyl.VectorTraits.Tests.Tuples {
+    [TestFixture()]
+    public class Vector128XTupleTests {
+#if NETCOREAPP3_0_OR_GREATER
+
+        [TestCase((float)1)]
+        [TestCase((double)2)]
+        [TestCase((sbyte)3)]
+        [TestCase((byte)4)]
+        [TestCase((short)5)]
+        [TestCase((ushort)6)]
+        [TestCase((int)7)]
+        [TestCase((uint)8)]
+        [TestCase((long)9)]
+        [TestCase((ulong)10)]
+        public void AsByteTest<T>(T src) where T : struct {
+            (Vector128<T>, Vector128<T>) tuple = (Vector128s<T>.Demo, Vector128s.Create<T>(src));
+            Vector128X2<T> vx = Vector128XTuple.Create(tuple);
+            Console.WriteLine($"Vector128XTuple.Create:\t{vx}");
+            Vector128X2<byte> vxByte = vx.AsByte();
+            Console.WriteLine($"AsByte:\t{vxByte}");
+            if (Vector<T>.Count == Vector128<T>.Count) {
+                VectorX2<byte> vScalable = vxByte.AsVector();
+                Console.WriteLine($"AsVector:\t{vScalable}");
+            }
+        }
+
+#endif // NETCOREAPP3_0_OR_GREATER
+    }
+}
