08b1f2ac759d3d0bcff65a4bfc2743218a234fae
zhouyuelin <zyl910@sina.com>
2023-01-06 23:12:04 +0800

    Update VectorTraits.Benchmarks.NetCore/VectorTraits.Benchmarks.NetFw, add AloneTest .

--- tests/VectorTraits.Benchmarks.NetCore/Program.cs
+++ tests/VectorTraits.Benchmarks.NetCore/Program.cs
@@ -1,26 +1,36 @@
 ﻿using System;
 using System.IO;
+using System.Runtime.InteropServices;
 
 namespace Zyl.VectorTraits.Benchmarks.NetCore {
     class Program {
         static void Main(string[] args) {
-            bool useBenchmark = false;
+            TextWriter writer = Console.Out;
+            writer.WriteLine("VectorTraits.Benchmarks.NetCore");
+            // benchmarkMode
+            // 0: Benchmark all with my BenchmarkMain.
+            // 3: Running special method (AloneTest).
+            int benchmarkMode = 0;
             if (args.Length >= 1) {
-                //bool.TryParse(args[0], out useBenchmark);
+                if (!int.TryParse(args[0], out benchmarkMode)) {
+                    benchmarkMode = 0;
+                }
             }
-            // run.
-            if (useBenchmark) {
-                //Summary summary = BenchmarkRunner.Run<ShiftLeftBenchmark_Int16>();
-                //var summary = BenchmarkRunner.Run(typeof(ShiftLeftBenchmark_Int16).Assembly);
+            writer.WriteLine("benchmarkMode:\t{0}", benchmarkMode);
+            if (benchmarkMode == 3) {
+                AloneTestUtil.AloneTest(writer);
+            //} else if (benchmarkMode > 0) {
+            //    Summary summary = BenchmarkRunner.Run<ShiftLeftBenchmark_Int16>();
+            //    var summary = BenchmarkRunner.Run(typeof(ShiftLeftBenchmark_Int16).Assembly);
             } else {
                 string indent = "";
-                TextWriter writer = Console.Out;
-                writer.WriteLine("VectorTraits.Benchmarks.NetCore");
                 writer.WriteLine();
                 BenchmarkUtil.OutputEnvironment(writer, indent);
                 TraitsOutput.ParseWaitDebugAndReadKey(args);
                 writer.WriteLine();
                 BenchmarkMain.RunBenchmark(writer, indent);
+                writer.WriteLine();
+                AloneTestUtil.AloneTest(writer);
             }
             //Console.ReadLine();
         }
--- tests/VectorTraits.Benchmarks.NetFw/Program.cs
+++ tests/VectorTraits.Benchmarks.NetFw/Program.cs
@@ -8,23 +8,32 @@ using System.Threading.Tasks;
 namespace Zyl.VectorTraits.Benchmarks.NetFw {
     class Program {
         static void Main(string[] args) {
-            bool useBenchmark = false;
+            TextWriter writer = Console.Out;
+            writer.WriteLine("VectorTraits.Benchmarks.NetFw");
+            // benchmarkMode
+            // 0: Benchmark all with my BenchmarkMain.
+            // 3: Running special method (AloneTest).
+            int benchmarkMode = 0;
             if (args.Length >= 1) {
-                //bool.TryParse(args[0], out useBenchmark);
+                if (!int.TryParse(args[0], out benchmarkMode)) {
+                    benchmarkMode = 0;
+                }
             }
-            // run.
-            if (useBenchmark) {
-                //Summary summary = BenchmarkRunner.Run<ShiftLeftBenchmark_Int16>();
-                //var summary = BenchmarkRunner.Run(typeof(ShiftLeftBenchmark_Int16).Assembly);
+            writer.WriteLine("benchmarkMode:\t{0}", benchmarkMode);
+            if (benchmarkMode == 3) {
+                AloneTestUtil.AloneTest(writer);
+                //} else if (benchmarkMode > 0) {
+                //    Summary summary = BenchmarkRunner.Run<ShiftLeftBenchmark_Int16>();
+                //    var summary = BenchmarkRunner.Run(typeof(ShiftLeftBenchmark_Int16).Assembly);
             } else {
                 string indent = "";
-                TextWriter writer = Console.Out;
-                writer.WriteLine("VectorTraits.Benchmarks.NetFw");
                 writer.WriteLine();
                 BenchmarkUtil.OutputEnvironment(writer, indent);
                 TraitsOutput.ParseWaitDebugAndReadKey(args);
                 writer.WriteLine();
                 BenchmarkMain.RunBenchmark(writer, indent);
+                writer.WriteLine();
+                AloneTestUtil.AloneTest(writer);
             }
             //Console.ReadLine();
         }
