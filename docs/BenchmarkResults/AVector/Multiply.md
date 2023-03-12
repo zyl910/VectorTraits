6c1e1f925821fb455a0636bd4e54ec5864a1db90
zhouyuelin <zyl910@sina.com>
2023-03-12 18:08:57 +0800

    Add MultiplyBenchmark_Double/MultiplyBenchmark_Int16/MultiplyBenchmark_Int32/MultiplyBenchmark_SByte/MultiplyBenchmark_Single .

--- docs/BenchmarkResults/AVector/Multiply.md
+++ docs/BenchmarkResults/AVector/Multiply.md
@@ -179,14 +179,78 @@ Vectors.Instance:	VectorTraits256Avx2
 Vectors.BaseInstance:	VectorTraits256Base
 
 
+[MultiplyBenchmark_Double(262144)]
+---
+NAME	US	MOPS	SCALE
+# SumMultiplyScalar	32712.618471138187
+Check-SumMultiplyBcl	Check `SumMultiplyBcl` mismatch. 32712.618471138107!=32712.618471138187
+Check-SumMultiplyVectorTraits	Check `SumMultiplyVectorTraits` mismatch. 32712.618471138107!=32712.618471138187
+Check-SumMultiplyVector128Traits	Check `SumMultiplyVector128Traits` mismatch. 32672.19972187258!=32712.618471138187
+Check-SumMultiplyVector256Traits	Check `SumMultiplyVector256Traits` mismatch. 32712.618471138107!=32712.618471138187
+SumMultiplyScalar	159.407	1644.493
+SumMultiplyBcl	40.304	6504.162	3.955117
+SumMultiplyVectorTraits	40.374	6492.811	3.948214
+SumMultiplyVector128Traits	79.789	3285.446	1.997847
+SumMultiplyVector256Traits	40.608	6455.493	3.925521
+
+[MultiplyBenchmark_Int16(262144)]
+---
+NAME	US	MOPS	SCALE
+# SumMultiplyScalar	-11701
+Check-SumMultiplyVector128Traits	Fail! Check `SumMultiplyVector128Traits` fail! 19479!=-11701
+SumMultiplyScalar	146.756	1786.263
+SumMultiplyBcl	9.020	29062.180	16.269825
+SumMultiplyVectorTraits	9.053	28956.092	16.210434
+SumMultiplyVector128Traits	12.363	21204.431	11.870836
+SumMultiplyVector256Traits	9.033	29022.226	16.247458
+
+[MultiplyBenchmark_Int32(262144)]
+---
+NAME	US	MOPS	SCALE
+# SumMultiplyScalar	-452441065
+Check-SumMultiplyVector128Traits	Fail! Check `SumMultiplyVector128Traits` fail! 575072105!=-452441065
+SumMultiplyScalar	120.914	2168.019
+SumMultiplyBcl	18.517	14157.296	6.530060
+SumMultiplyVectorTraits	18.475	14189.488	6.544909
+SumMultiplyVector128Traits	26.336	9953.966	4.591272
+SumMultiplyVector256Traits	20.416	12840.107	5.922506
+
+[MultiplyBenchmark_SByte(262144)]
+---
+NAME	US	MOPS	SCALE
+# SumMultiplyScalar	-41
+Check-SumMultiplyVector128Traits	Fail! Check `SumMultiplyVector128Traits` fail! -113!=-41
+SumMultiplyScalar	141.677	1850.298
+SumMultiplyBcl	130.501	2008.757	1.085640
+SumMultiplyVectorTraits	130.761	2004.759	1.083479
+SumMultiplyVector128Traits	155.413	1686.758	0.911614
+SumMultiplyVector256Traits	131.049	2000.349	1.081095
+
+[MultiplyBenchmark_Single(262144)]
+---
+NAME	US	MOPS	SCALE
+# SumMultiplyScalar	32717.883
+Check-SumMultiplyBcl	Check `SumMultiplyBcl` mismatch. 32717.988!=32717.883
+Check-SumMultiplyVectorTraits	Check `SumMultiplyVectorTraits` mismatch. 32717.988!=32717.883
+Check-SumMultiplyVector128Traits	Check `SumMultiplyVector128Traits` mismatch. 32712.596!=32717.883
+Check-SumMultiplyVector256Traits	Check `SumMultiplyVector256Traits` mismatch. 32717.988!=32717.883
+SumMultiplyScalar	156.679	1673.124
+SumMultiplyBcl	20.028	13088.803	7.822973
+SumMultiplyVectorTraits	20.902	12541.319	7.495750
+SumMultiplyVector128Traits	39.837	6580.435	3.933023
+SumMultiplyVector256Traits	20.194	12981.279	7.758707
+
 [MultiplyBenchmark_UInt64(262144)]
 ---
 NAME	US	MOPS	SCALE
 # SumMultiplyScalar	12001033377966188484
-SumMultiplyScalar	141.807	1848.596
-SumMultiplyBcl	248.472	1055.023	0.570716
-SumMultiplyVector256Traits	80.734	3247.018	1.756479
-SumMultiplyVector256Avx	81.184	3228.993	1.746728
+Check-SumMultiplyVector128Traits	Fail! Check `SumMultiplyVector128Traits` fail! 12862679336949715101!=12001033377966188484
+SumMultiplyScalar	137.860	1901.517
+SumMultiplyBcl	253.089	1035.779	0.544712
+SumMultiplyVectorTraits	81.055	3234.145	1.700824
+SumMultiplyVector128Traits	405.564	646.369	0.339923
+SumMultiplyVector256Traits	81.096	3232.498	1.699958
+SumMultiplyVector256Avx	81.429	3219.295	1.693014
 
 VectorTraitsBase: OK.
 VectorTraits128Base: Vector byte size mismatch(32!=16) !
---
+++ tests/VectorTraits.Benchmarks.Inc/AVector/M/MultiplyBenchmark_Int16.cs
@@ -0,0 +1,264 @@
+﻿#undef BENCHMARKS_OFF
+
+using BenchmarkDotNet.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Numerics;
+using System.Runtime.CompilerServices;
+#if NETCOREAPP3_0_OR_GREATER
+using System.Runtime.Intrinsics;
+#endif
+
+namespace Zyl.VectorTraits.Benchmarks.AVector.M {
+#if BENCHMARKS_OFF
+    using BenchmarkAttribute = FakeBenchmarkAttribute;
+#else
+#endif // BENCHMARKS_OFF
+
+    // My type.
+    using TMy = Int16;
+
+    /// <summary>
+    /// Multiply benchmark - Int16.
+    /// </summary>
+#if NETCOREAPP3_0_OR_GREATER && DRY_JOB
+    [DryJob]
+#endif // NETCOREAPP3_0_OR_GREATER && DRY_JOB
+    public partial class MultiplyBenchmark_Int16 : AbstractSharedBenchmark_Int16 {
+
+        // -- var --
+
+        /// <summary>
+        /// Sum Multiply - Scalar.
+        /// </summary>
+        /// <param name="src">Source array.</param>
+        /// <param name="srcCount">Source count</param>
+        /// <returns>Returns the sum.</returns>
+        public static TMy StaticSumMultiplyScalar(TMy[] src, int srcCount) {
+            TMy rt = 0; // Result.
+            const int GroupSize = 2;
+            int VectorWidth = Vector<TMy>.Count; // Block width.
+            int nBlockWidth = VectorWidth * GroupSize; // Block width.
+            int cntBlock = srcCount / nBlockWidth; // Block count.
+            int p = 0;
+            for (int i = 0; i < cntBlock; ++i) {
+                for (int j = 0; j < VectorWidth; ++j) {
+                    rt += (TMy)(src[p + j] * src[p + j + VectorWidth]);
+                }
+                p += nBlockWidth;
+            }
+            return rt;
+        }
+
+        [Benchmark(Baseline = true)]
+        public void SumMultiplyScalar() {
+            dstTMy = StaticSumMultiplyScalar(srcArray, srcArray.Length);
+            if (CheckMode) {
+                baselineTMy = dstTMy;
+                BenchmarkUtil.WriteItem("# SumMultiplyScalar", string.Format("{0}", baselineTMy));
+            }
+        }
+
+        /// <summary>
+        /// Sum Multiply - BCL.
+        /// </summary>
+        /// <param name="src">Source array.</param>
+        /// <param name="srcCount">Source count</param>
+        /// <returns>Returns the sum.</returns>
+        public static TMy StaticSumMultiplyBcl(TMy[] src, int srcCount) {
+            TMy rt = 0; // Result.
+            const int GroupSize = 2;
+            int VectorWidth = Vector<TMy>.Count; // Block width.
+            int nBlockWidth = VectorWidth * GroupSize; // Block width.
+            int cntBlock = srcCount / nBlockWidth; // Block count.
+            //int cntRem = srcCount % nBlockWidth; // Remainder count.
+            Vector<TMy> vrt = Vector<TMy>.Zero; // Vector result.
+            int i;
+            // Body.
+            ref Vector<TMy> p0 = ref Unsafe.As<TMy, Vector<TMy>>(ref src[0]);
+            // a) Vector processs.
+            for (i = 0; i < cntBlock; ++i) {
+                vrt += Vector.Multiply(p0, Unsafe.Add(ref p0, 1));
+                p0 = ref Unsafe.Add(ref p0, GroupSize);
+            }
+            // b) Remainder processs.
+            //ref TMy p = ref Unsafe.As<Vector<TMy>, TMy>(ref p0);
+            //for (i = 0; i < cntRem; ++i) {
+            //    rt += (TMy)Unsafe.Add(ref p, i);
+            //}
+            // Reduce.
+            for (i = 0; i < Vector<TMy>.Count; ++i) {
+                rt += vrt[i];
+            }
+            return rt;
+        }
+
+        [Benchmark]
+        public void SumMultiplyBcl() {
+            dstTMy = StaticSumMultiplyBcl(srcArray, srcArray.Length);
+            CheckResult("SumMultiplyBcl");
+        }
+
+        #region BENCHMARKS_RAW
+#if BENCHMARKS_RAW
+
+
+#endif // BENCHMARKS_RAW
+        #endregion // BENCHMARKS_RAW
+
+
+        /// <summary>
+        /// Sum Multiply - Vector Traits - static.
+        /// </summary>
+        /// <param name="src">Source array.</param>
+        /// <param name="srcCount">Source count</param>
+        /// <returns>Returns the sum.</returns>
+        public static TMy StaticSumMultiplyVectorTraits(TMy[] src, int srcCount) {
+            TMy rt = 0; // Result.
+            const int GroupSize = 2;
+            int VectorWidth = Vector<TMy>.Count; // Block width.
+            int nBlockWidth = VectorWidth * GroupSize; // Block width.
+            int cntBlock = srcCount / nBlockWidth; // Block count.
+            //int cntRem = srcCount % nBlockWidth; // Remainder count.
+            Vector<TMy> vrt = Vector<TMy>.Zero; // Vector result.
+            int i;
+            // Body.
+            ref Vector<TMy> p0 = ref Unsafe.As<TMy, Vector<TMy>>(ref src[0]);
+            // a) Vector processs.
+            for (i = 0; i < cntBlock; ++i) {
+                vrt += Vectors.Multiply(p0, Unsafe.Add(ref p0, 1));
+                p0 = ref Unsafe.Add(ref p0, GroupSize);
+            }
+            // b) Remainder processs.
+            //ref TMy p = ref Unsafe.As<Vector<TMy>, TMy>(ref p0);
+            //for (i = 0; i < cntRem; ++i) {
+            //    rt += (TMy)Unsafe.Add(ref p, i);
+            //}
+            // Reduce.
+            for (i = 0; i < Vector<TMy>.Count; ++i) {
+                rt += vrt[i];
+            }
+            return rt;
+        }
+
+        [Benchmark]
+        public void SumMultiplyVectorTraits() {
+            dstTMy = StaticSumMultiplyVectorTraits(srcArray, srcArray.Length);
+            CheckResult("SumMultiplyVectorTraits");
+        }
+
+
+#if NETCOREAPP3_0_OR_GREATER
+
+        #region BENCHMARKS_128ALGORITHM
+#if BENCHMARKS_128ALGORITHM
+
+        /// <summary>
+        /// Sum Multiply - Vector128 - Traits static.
+        /// </summary>
+        /// <param name="src">Source array.</param>
+        /// <param name="srcCount">Source count</param>
+        /// <returns>Returns the sum.</returns>
+        public static TMy StaticSumMultiplyVector128Traits(TMy[] src, int srcCount) {
+            TMy rt = 0; // Result.
+            const int GroupSize = 2;
+            int VectorWidth = Vector128<TMy>.Count; // Block width.
+            int nBlockWidth = VectorWidth * GroupSize; // Block width.
+            int cntBlock = srcCount / nBlockWidth; // Block count.
+            //int cntRem = srcCount % nBlockWidth; // Remainder count.
+            Vector128<TMy> vrt = Vector128<TMy>.Zero; // Vector128 result.
+            Vector128<TMy> t;
+            int i;
+            // Body.
+            ref Vector128<TMy> p0 = ref Unsafe.As<TMy, Vector128<TMy>>(ref src[0]);
+            // a) Vector128 processs.
+            for (i = 0; i < cntBlock; ++i) {
+                t = Vector128s.Multiply(p0, Unsafe.Add(ref p0, 1));
+                vrt = Vector128s.Add(vrt, t);
+                p0 = ref Unsafe.Add(ref p0, GroupSize);
+            }
+            // b) Remainder processs.
+            //ref TMy p = ref Unsafe.As<Vector128<TMy>, TMy>(ref p0);
+            //for (i = 0; i < cntRem; ++i) {
+            //    rt += (TMy)Unsafe.Add(ref p, i);
+            //}
+            // Reduce.
+            for (i = 0; i < Vector128<TMy>.Count; ++i) {
+                rt += vrt.GetElement(i);
+            }
+            return rt;
+        }
+
+        [Benchmark]
+        public void SumMultiplyVector128Traits() {
+            Vector128s.ThrowForUnsupported(true);
+            dstTMy = StaticSumMultiplyVector128Traits(srcArray, srcArray.Length);
+            CheckResult("SumMultiplyVector128Traits");
+        }
+
+        #region BENCHMARKS_ALGORITHM
+#if BENCHMARKS_ALGORITHM
+#if NET5_0_OR_GREATER
+
+#endif // NET5_0_OR_GREATER
+
+#endif // BENCHMARKS_ALGORITHM
+        #endregion // BENCHMARKS_ALGORITHM
+
+
+#endif // BENCHMARKS_128ALGORITHM
+        #endregion // BENCHMARKS_128ALGORITHM
+
+        #region BENCHMARKS_256ALGORITHM
+#if BENCHMARKS_256ALGORITHM
+
+        /// <summary>
+        /// Sum Multiply - Vector256 - Traits static.
+        /// </summary>
+        /// <param name="src">Source array.</param>
+        /// <param name="srcCount">Source count</param>
+        /// <returns>Returns the sum.</returns>
+        public static TMy StaticSumMultiplyVector256Traits(TMy[] src, int srcCount) {
+            TMy rt = 0; // Result.
+            const int GroupSize = 2;
+            int VectorWidth = Vector256<TMy>.Count; // Block width.
+            int nBlockWidth = VectorWidth * GroupSize; // Block width.
+            int cntBlock = srcCount / nBlockWidth; // Block count.
+            //int cntRem = srcCount % nBlockWidth; // Remainder count.
+            Vector256<TMy> vrt = Vector256<TMy>.Zero; // Vector256 result.
+            Vector256<TMy> t;
+            int i;
+            // Body.
+            ref Vector256<TMy> p0 = ref Unsafe.As<TMy, Vector256<TMy>>(ref src[0]);
+            // a) Vector256 processs.
+            for (i = 0; i < cntBlock; ++i) {
+                t = Vector256s.Multiply(p0, Unsafe.Add(ref p0, 1));
+                vrt = Vector256s.Add(vrt, t);
+                p0 = ref Unsafe.Add(ref p0, GroupSize);
+            }
+            // b) Remainder processs.
+            //ref TMy p = ref Unsafe.As<Vector256<TMy>, TMy>(ref p0);
+            //for (i = 0; i < cntRem; ++i) {
+            //    rt += (TMy)Unsafe.Add(ref p, i);
+            //}
+            // Reduce.
+            for (i = 0; i < Vector256<TMy>.Count; ++i) {
+                rt += vrt.GetElement(i);
+            }
+            return rt;
+        }
+
+        [Benchmark]
+        public void SumMultiplyVector256Traits() {
+            Vector256s.ThrowForUnsupported(true);
+            dstTMy = StaticSumMultiplyVector256Traits(srcArray, srcArray.Length);
+            CheckResult("SumMultiplyVector256Traits");
+        }
+
+#endif // BENCHMARKS_256ALGORITHM
+        #endregion // BENCHMARKS_256ALGORITHM
+
+#endif // NETCOREAPP3_0_OR_GREATER
+    }
+}
