f804823f7af8b5e3314d406eb2952ed7d999726e
zhouyuelin <zyl910@sina.com>
2023-06-03 22:39:38 +0800

    Update RunBenchmarks.bat

--- tests/RunBenchmarks.bat
+++ tests/RunBenchmarks.bat
@@ -1,9 +1,70 @@
 @REM Put me to `VectorTraits/tests/`
 cd /d "%~dp0"
+@ECHO OFF
 
-start /realtime /wait /b VectorTraits.Benchmarks/bin/Release/netcoreapp3.1/VectorTraits.Benchmarks.exe >netcore3.1.txt
-start /realtime /wait /b VectorTraits.Benchmarks/bin/Release/net5.0/VectorTraits.Benchmarks.exe >net5.0.txt
-start /realtime /wait /b VectorTraits.Benchmarks/bin/Release/net6.0/VectorTraits.Benchmarks.exe >net6.0.txt
-start /realtime /wait /b VectorTraits.Benchmarks/bin/Release/net7.0/VectorTraits.Benchmarks.exe >net7.0.txt
+:Check_netcore3_1
+SET my_path=netcoreapp3.1/VectorTraits.Benchmarks.exe
+IF EXIST "%my_path%" GOTO Run_netcore3_1
+SET my_path=Release/netcoreapp3.1/VectorTraits.Benchmarks.exe
+IF EXIST "%my_path%" GOTO Run_netcore3_1
+SET my_path=bin/Release/netcoreapp3.1/VectorTraits.Benchmarks.exe
+IF EXIST "%my_path%" GOTO Run_netcore3_1
+SET my_path=VectorTraits.Benchmarks/bin/Release/netcoreapp3.1/VectorTraits.Benchmarks.exe
+IF EXIST "%my_path%" GOTO Run_netcore3_1
+GOTO Done_netcore3_1
+:Run_netcore3_1
+@ECHO ON
+start /realtime /wait /b %my_path% >netcore3.1.txt
+@ECHO OFF
+:Done_netcore3_1
 
+:Check_net5_0
+SET my_path=net5.0/VectorTraits.Benchmarks.exe
+IF EXIST "%my_path%" GOTO Run_net5_0
+SET my_path=Release/net5.0/VectorTraits.Benchmarks.exe
+IF EXIST "%my_path%" GOTO Run_net5_0
+SET my_path=bin/Release/net5.0/VectorTraits.Benchmarks.exe
+IF EXIST "%my_path%" GOTO Run_net5_0
+SET my_path=VectorTraits.Benchmarks/bin/Release/net5.0/VectorTraits.Benchmarks.exe
+IF EXIST "%my_path%" GOTO Run_net5_0
+GOTO Done_net5_0
+:Run_net5_0
+@ECHO ON
+start /realtime /wait /b %my_path% >net5.0.txt
+@ECHO OFF
+:Done_net5_0
+
+:Check_net6_0
+SET my_path=net6.0/VectorTraits.Benchmarks.exe
+IF EXIST "%my_path%" GOTO Run_net6_0
+SET my_path=Release/net6.0/VectorTraits.Benchmarks.exe
+IF EXIST "%my_path%" GOTO Run_net6_0
+SET my_path=bin/Release/net6.0/VectorTraits.Benchmarks.exe
+IF EXIST "%my_path%" GOTO Run_net6_0
+SET my_path=VectorTraits.Benchmarks/bin/Release/net6.0/VectorTraits.Benchmarks.exe
+IF EXIST "%my_path%" GOTO Run_net6_0
+GOTO Done_net6_0
+:Run_net6_0
+@ECHO ON
+start /realtime /wait /b %my_path% >net6.0.txt
+@ECHO OFF
+:Done_net6_0
+
+:Check_net7_0
+SET my_path=net7.0/VectorTraits.Benchmarks.exe
+IF EXIST "%my_path%" GOTO Run_net7_0
+SET my_path=Release/net7.0/VectorTraits.Benchmarks.exe
+IF EXIST "%my_path%" GOTO Run_net7_0
+SET my_path=bin/Release/net7.0/VectorTraits.Benchmarks.exe
+IF EXIST "%my_path%" GOTO Run_net7_0
+SET my_path=VectorTraits.Benchmarks/bin/Release/net7.0/VectorTraits.Benchmarks.exe
+IF EXIST "%my_path%" GOTO Run_net7_0
+GOTO Done_net7_0
+:Run_net7_0
+@ECHO ON
+start /realtime /wait /b %my_path% >net7.0.txt
+@ECHO OFF
+:Done_net7_0
+
+@ECHO ON
 timeout /T 60
--- tests/RunBenchmarks_All.bat
+++ tests/RunBenchmarks_All.bat
@@ -1,7 +1,41 @@
 @REM Put me to `VectorTraits/tests/`
 cd /d "%~dp0"
+@ECHO OFF
 
-start /realtime /wait /b VectorTraits.Benchmarks.NetFw/bin/Release/VectorTraits.Benchmarks.NetFw.exe >netFw.txt
-start /realtime /wait /b dotnet VectorTraits.Benchmarks.NetCore/bin/Release/netcoreapp2.1/VectorTraits.Benchmarks.NetCore.dll >netcore2.1.txt
+:Check_netFw
+SET my_path=VectorTraits.Benchmarks.NetFw.exe
+IF EXIST "%my_path%" GOTO Run_netFw
+SET my_path=Release/VectorTraits.Benchmarks.NetFw.exe
+IF EXIST "%my_path%" GOTO Run_netFw
+SET my_path=Release/Release/VectorTraits.Benchmarks.NetFw.exe
+IF EXIST "%my_path%" GOTO Run_netFw
+SET my_path=bin/Release/Release/VectorTraits.Benchmarks.NetFw.exe
+IF EXIST "%my_path%" GOTO Run_netFw
+SET my_path=VectorTraits.Benchmarks.NetFw/bin/Release/VectorTraits.Benchmarks.NetFw.exe
+IF EXIST "%my_path%" GOTO Run_netFw
+GOTO Done_netFw
+:Run_netFw
+@ECHO ON
+start /realtime /wait /b %my_path% >netFw.txt
+@ECHO OFF
+:Done_netFw
+
+:Check_netcore2_1
+SET my_path=VectorTraits.Benchmarks.NetCore.dll
+IF EXIST "%my_path%" GOTO Run_netcore2_1
+SET my_path=netcoreapp2.1/VectorTraits.Benchmarks.NetCore.dll
+IF EXIST "%my_path%" GOTO Run_netcore2_1
+SET my_path=Release/netcoreapp2.1/VectorTraits.Benchmarks.NetCore.dll
+IF EXIST "%my_path%" GOTO Run_netcore2_1
+SET my_path=bin/Release/netcoreapp2.1/VectorTraits.Benchmarks.NetCore.dll
+IF EXIST "%my_path%" GOTO Run_netcore2_1
+SET my_path=VectorTraits.Benchmarks.NetCore/bin/Release/netcoreapp2.1/VectorTraits.Benchmarks.NetCore.dll
+IF EXIST "%my_path%" GOTO Run_netcore2_1
+GOTO Done_netcore2_1
+:Run_netcore2_1
+@ECHO ON
+start /realtime /wait /b dotnet %my_path% >netcore2.1.txt
+@ECHO OFF
+:Done_netcore2_1
 
 RunBenchmarks.bat
