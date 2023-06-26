b1505815ca0ae2e3dbd992440f71542fadf33843
zhouyuelin <zyl910@sina.com>
2023-06-26 15:37:49 +0800

    ReflectionUtil add CheckBindMethods methods; Add ReflectionUtilTests .

--- src/VectorTraits/Impl/ReflectionUtil.cs
+++ src/VectorTraits/Impl/ReflectionUtil.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
+using Zyl.VectorTraits.Impl.AVector;
 
 namespace Zyl.VectorTraits.Impl {
     /// <summary>
@@ -161,6 +163,139 @@ namespace Zyl.VectorTraits.Impl {
             int cnt = GetSupportedMethodListFill(rt, types, methodNames);
             if (cnt > 0) {
             }
+            return rt;
+        }
+
+        /// <summary>
+        /// Get methods group (取得方法组).
+        /// </summary>
+        /// <param name="aType">The type (类型).</param>
+        /// <returns>Returns methods group (返回方法组).</returns>
+        public static SortedDictionary<string, List<MethodInfo>> GetMethodGroup(Type aType) {
+            var methods = aType.GetRuntimeMethods();
+            var groupBy = methods.GroupBy(o => o.Name)
+                .ToDictionary(g => g.Key, g => g.ToList());
+            var dict = new SortedDictionary<string, List<MethodInfo>>(groupBy);
+            return dict;
+        }
+
+        /// <summary>
+        /// Check if the parameter list of the methods is the same.
+        /// </summary>
+        /// <param name="a">The first value (第一个值).</param>
+        /// <param name="b">The second value (第二个值).</param>
+        /// <returns>Returns true if equal, otherwise false (若相等便返回true, 否则返回false).</returns>
+        public static bool EqualsParametersType(MethodInfo a, MethodInfo b) {
+            if (a == b) return true;
+            if (null == a) return false;
+            if (null == b) return false;
+            ParameterInfo[] parameters1 = a.GetParameters();
+            ParameterInfo[] parameters2 = b.GetParameters();
+            if (parameters1.Length != parameters2.Length) {
+                return false;
+            }
+            for (int i = 0; i < parameters1.Length; i++) {
+                Type parameterType1 = parameters1[i].ParameterType;
+                Type parameterType2 = parameters2[i].ParameterType;
+                if (!parameterType1.Equals(parameterType2)) {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// Default of OnMissed functon (默认的 OnMissed 函数).
+        /// </summary>
+        /// <param name="methodStatic">The method of a static class (静态类的方法).</param>
+        /// <param name="methodInterface">The method of a static class (接口的方法).</param>
+        /// <param name="userdata">The userdata.</param>
+        /// <returns>Returns true if you want to ignore <paramref name="methodStatic"/>, false otherwise (若需忽略 <paramref name="methodStatic"/>，便返回true, 否则会返回false).</returns>
+        /// <seealso cref="CheckBindMethods"/>
+        public static bool OnMissed_Default(MethodInfo methodStatic, MethodInfo? methodInterface, object? userdata) {
+            bool isIgnore = (null == methodInterface); // If it is not in the method list of the interface, it is ignored.
+            _ = methodStatic;
+            _ = userdata;
+            return isIgnore;
+        }
+
+        /// <summary>
+        /// Check the binding of the method set on <paramref name="funcEquals"/> (基于 <paramref name="funcEquals"/> 检查方法集的绑定.
+        /// </summary>
+        /// <param name="funcEquals">The callback function to determine equality (判断相等的回调函数). Prototype: <c>bool isEquals = funcEquals(MethodInfo a, MethodInfo b)</c>. e.g. <see cref="EqualsParametersType"/> </param>
+        /// <param name="staticType">The type of a static class (静态类的类型).</param>
+        /// <param name="objectType">The type of a object class (对象类的类型).</param>
+        /// <param name="interfaceMethodsDictionary">The methods of interface (接口类型的方法集合). If it is not null, the <paramref name="staticType"/> is checked in the scope of the method set of the interface (如果它非空，则在接口的方法集范围内检查 <paramref name="staticType"/> ).</param>
+        /// <param name="onMissed">The callback function for finding a missed method (找到未命中方法时的回调函数). Prototype: <c>bool isIgnore = onMissed(MethodInfo methodStatic, MethodInfo? methodInterface, object? userdata)</c>. If it is null, <see cref="OnMissed_Default"/> will be used.</param>
+        /// <param name="userdata">The userdata.</param>
+        /// <returns>Returns the number of missed methods. Specifically, the number of times when <paramref name="onMissed"/> returns false (返回未命中方法的个数。具体来说，是 <paramref name="onMissed"/> 返回false的次数).</returns>
+        public static int CheckBindMethodsOn(Func<MethodInfo, MethodInfo, bool> funcEquals, Type staticType, Type objectType, IDictionary<string, List<MethodInfo>>? interfaceMethodsDictionary, Func<MethodInfo, MethodInfo?, object?, bool>? onMissed = null, object? userdata = null) {
+            int rt = 0;
+            if (null == onMissed) onMissed = OnMissed_Default;
+            var staticDictionary = GetMethodGroup(staticType);
+            var objectDictionary = objectType.GetRuntimeMethods().GroupBy(o => o.Name)
+                .ToDictionary(g => g.Key, g => g.ToList());
+            foreach (KeyValuePair<string, List<MethodInfo>> kvp in staticDictionary) {
+                string methodName = kvp.Key;
+                if (null == kvp.Value || kvp.Value.Count <= 0) continue;
+                List<MethodInfo>? objectMethods;
+                if (!objectDictionary.TryGetValue(methodName, out objectMethods)) objectMethods = null;
+                List<MethodInfo>? interfaceMethods = null;
+                if (null != interfaceMethodsDictionary) {
+                    if (!interfaceMethodsDictionary.TryGetValue(methodName, out interfaceMethods)) interfaceMethods = null;
+                }
+                // methodStatic.
+                foreach (MethodInfo? methodStatic in kvp.Value) {
+                    if (null == methodStatic) continue;
+                    MethodInfo? objectMethod = null;
+                    if (null != objectMethods) {
+                        objectMethod = objectMethods.FirstOrDefault(x => funcEquals(methodStatic, x));
+                    }
+                    bool isFound = null != objectMethod;
+                    if (!isFound) {
+                        MethodInfo? interfaceMethod = null;
+                        if (null != interfaceMethods) {
+                            interfaceMethod = interfaceMethods.FirstOrDefault(x => funcEquals(methodStatic, x));
+                        }
+                        bool isIgnore = onMissed(methodStatic, interfaceMethod, userdata);
+                        if (!isIgnore) {
+                            ++rt;
+                        }
+                    }
+                }
+            }
+            return rt;
+        }
+
+        /// <summary>
+        /// Check the binding of the method set, i.e. whether the method of the <paramref name="staticType"/> is missing from the <paramref name="objectType"/> (检查方法集的绑定，即在 <paramref name="objectType"/> 中是否缺少 <paramref name="staticType"/> 的方法). e.g. <see cref="VectorTraitsBase.Statics"/> and <see cref="VectorTraitsAbstract"/> . It has the interfaceMethodsDictionary parameter.
+        /// </summary>
+        /// <param name="staticType">The type of a static class (静态类的类型).</param>
+        /// <param name="objectType">The type of a object class (对象类的类型).</param>
+        /// <param name="interfaceMethodsDictionary">The methods of interface (接口类型的方法集合). If it is not null, the <paramref name="staticType"/> is checked in the scope of the method set of the interface (如果它非空，则在接口的方法集范围内检查 <paramref name="staticType"/> ).</param>
+        /// <param name="onMissed">The callback function for finding a missed method (找到未命中方法时的回调函数). Prototype: <c>bool isIgnore = onMissed(MethodInfo methodStatic, MethodInfo? methodInterface, object? userdata)</c>. If it is null, <see cref="OnMissed_Default"/> will be used.</param>
+        /// <param name="userdata">The userdata.</param>
+        /// <returns>Returns the number of missed methods. Specifically, the number of times when <paramref name="onMissed"/> returns false (返回未命中方法的个数。具体来说，是 <paramref name="onMissed"/> 返回false的次数).</returns>
+        public static int CheckBindMethods(Type staticType, Type objectType, IDictionary<string, List<MethodInfo>>? interfaceMethodsDictionary, Func<MethodInfo, MethodInfo?, object?, bool>? onMissed = null, object? userdata = null) {
+            int rt = CheckBindMethodsOn(EqualsParametersType, staticType, objectType, interfaceMethodsDictionary, onMissed, userdata);
+            return rt;
+        }
+
+        /// <summary>
+        /// Check the binding of the method set, i.e. whether the method of the <paramref name="staticType"/> is missing from the <paramref name="objectType"/> (检查方法集的绑定，即在 <paramref name="objectType"/> 中是否缺少 <paramref name="staticType"/> 的方法). e.g. <see cref="VectorTraitsBase.Statics"/> and <see cref="VectorTraitsAbstract"/> .
+        /// </summary>
+        /// <param name="staticType">The type of a static class (静态类的类型).</param>
+        /// <param name="objectType">The type of a object class (对象类的类型).</param>
+        /// <param name="interfaceType">The type of interface (接口的类型). If it is not null, the <paramref name="staticType"/> is checked in the scope of the method set of the interface (如果它非空，则在接口的方法集范围内检查 <paramref name="staticType"/> ).</param>
+        /// <param name="onMissed">The callback function for finding a missed method (找到未命中方法时的回调函数). Prototype: <c>bool isIgnore = onMissed(MethodInfo methodStatic, MethodInfo? methodInterface, object? userdata)</c>. If it is null, <see cref="OnMissed_Default"/> will be used.</param>
+        /// <param name="userdata">The userdata.</param>
+        /// <returns>Returns the number of missed methods. Specifically, the number of times when <paramref name="onMissed"/> returns false (返回未命中方法的个数。具体来说，是 <paramref name="onMissed"/> 返回false的次数).</returns>
+        public static int CheckBindMethods(Type staticType, Type objectType, Type? interfaceType, Func<MethodInfo, MethodInfo?, object?, bool>? onMissed = null, object? userdata = null) {
+            SortedDictionary<string, List<MethodInfo>>? interfaceMethodsDictionary = null;
+            if (null != interfaceType) {
+                interfaceMethodsDictionary = GetMethodGroup(interfaceType);
+            }
+            int rt = CheckBindMethods(staticType, objectType, interfaceMethodsDictionary, onMissed, userdata);
             return rt;
         }
 
---
+++ tests/VectorTraits.Tests/Impl/ReflectionUtilTests.cs
@@ -0,0 +1,46 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Zyl.VectorTraits;
+using Zyl.VectorTraits.Impl;
+using Zyl.VectorTraits.Impl.AVector;
+using System.Reflection;
+
+namespace Zyl.VectorTraits.Tests.Impl {
+    [TestFixture()]
+    public class ReflectionUtilTests {
+
+        [Test()]
+        public void CheckBindMethodsTest() {
+            TextWriter writer = Console.Out;
+            Type? interfaceType = null;
+            interfaceType = typeof(IVectorTraits);
+            Type[] types = new Type[] {
+                typeof(VectorTraitsBase.Statics), typeof(VectorTraitsAbstract),
+                typeof(VectorTraits128Base.Statics), typeof(VectorTraits128Abstract),
+                typeof(VectorTraits128AdvSimd.Statics), typeof(VectorTraits128AdvSimdAbstract),
+                typeof(VectorTraits128AdvSimdB64.Statics), typeof(VectorTraits128AdvSimdB64Abstract),
+                typeof(VectorTraits256Base.Statics), typeof(VectorTraits256Abstract),
+                typeof(VectorTraits256Avx2.Statics), typeof(VectorTraits256Avx2Abstract),
+            };
+            for (int i = 0; i < types.Length; i += 2) {
+                Type staticType = types[i];
+                Type objectType = types[i + 1];
+                writer.WriteLine("{0}:", staticType.FullName);
+                int n = ReflectionUtil.CheckBindMethods(staticType, objectType, interfaceType, delegate (MethodInfo methodStatic, MethodInfo? methodInterface, object? userdata) {
+                    bool isIgnore = (null == methodInterface); // If it is not in the method list of the interface, it is ignored.
+                    _ = userdata;
+                    if (!isIgnore) {
+                        writer.WriteLine("  {0}", methodStatic.ToString());
+                    }
+                    return isIgnore;
+                });
+                writer.WriteLine("{0} missed count: {1}", staticType.FullName, n);
+            }
+        }
+
+
+    }
+}
