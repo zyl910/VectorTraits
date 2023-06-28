bb24e052634dcd76b2523a7b3287423071de1638
zhouyuelin <zyl910@sina.com>
2023-06-28 17:37:07 +0800

    Revised documentation comments for ScalarConstants: BitDouble_2Pow52, BitDouble_2Pow52_2Pow51 .

--- src/VectorTraits/Impl/AVector256/WVectorTraits256Avx2.cs
+++ src/VectorTraits/Impl/AVector256/WVectorTraits256Avx2.cs
@@ -220,7 +220,7 @@ namespace Zyl.VectorTraits.Impl.AVector256 {
                 //     x = _mm256_add_epi64(x, _mm256_castpd_si256(_mm256_set1_pd(0x0018000000000000)));
                 //     return _mm256_sub_pd(_mm256_castsi256_pd(x), _mm256_set1_pd(0x0018000000000000));
                 // }
-                // [Bug number] Vector256<long> magicNumber = Vector256.Create(0x00180000_00000000L); // BitConverter.DoubleToInt64Bits(1.5*Math.Pow(2, -1022)).ToString("X")
+                // BitConverter.DoubleToInt64Bits((double)0x0018000000000000).ToString("X") = "4338000000000000"
                 Vector256<long> magicNumber = Vector256.Create(ScalarConstants.BitDouble_2Pow52_2Pow51); // Double value: 1.5*pow(2, 52) = pow(2, 52) + pow(2, 51)
                 Vector256<long> x = Avx2.Add(value, magicNumber);
                 Vector256<double> result = Avx.Subtract(x.AsDouble(), magicNumber.AsDouble());
@@ -293,7 +293,7 @@ namespace Zyl.VectorTraits.Impl.AVector256 {
                 //     x = _mm256_or_si256(x, _mm256_castpd_si256(_mm256_set1_pd(0x0010000000000000)));
                 //     return _mm256_sub_pd(_mm256_castsi256_pd(x), _mm256_set1_pd(0x0010000000000000));
                 // }
-                // [Bug number] Vector256<ulong> magicNumber = Vector256.Create(0x00100000_00000000UL); // BitConverter.DoubleToInt64Bits(Math.Pow(2, -1022)).ToString("X")
+                // BitConverter.DoubleToInt64Bits((double)0x0010000000000000).ToString("X") = "4330000000000000"
                 Vector256<ulong> magicNumber = Vector256.Create((ulong)ScalarConstants.BitDouble_2Pow52); // Double value: pow(2, 52)
                 Vector256<ulong> x = Avx2.Or(value, magicNumber);
                 Vector256<double> result = Avx.Subtract(x.AsDouble(), magicNumber.AsDouble());
--- src/VectorTraits/Impl/ScalarConstants.cs
+++ src/VectorTraits/Impl/ScalarConstants.cs
@@ -9,9 +9,9 @@ namespace Zyl.VectorTraits.Impl {
     public static class ScalarConstants {
 
         // == BitDouble: Bit of Double ==
-        /// <summary>BitDouble - `pow(2, 52) = 4503599627370496`.</summary>
+        /// <summary>BitDouble - `(double)0x0010000000000000 = pow(2, 52) = 4503599627370496`.</summary>
         public const long BitDouble_2Pow52 = 0x43300000_00000000; // BitConverter.DoubleToInt64Bits(Math.Pow(2, 52)).ToString("X")
-        /// <summary>BitDouble - `1.5*pow(2, 52) = pow(2, 52) + pow(2, 51) = 6755399441055744`.</summary>
+        /// <summary>BitDouble - `(double)0x0018000000000000 = pow(2, 52) + pow(2, 51) = 1.5*pow(2, 52) = 6755399441055744`.</summary>
         public const long BitDouble_2Pow52_2Pow51 = 0x43380000_00000000L; // BitConverter.DoubleToInt64Bits(1.5*Math.Pow(2, 52)).ToString("X")
 
         // == BitSingle: Bit of Single ==
