ea281eedf8f7e77b8894d787d14b2255e2ac516d
zhouyuelin <zyl910@sina.com>
2023-07-02 23:11:24 +0800

    Add MathTests

---
+++ tests/VectorTraits.Tests/MathTests.cs
@@ -0,0 +1,64 @@
+﻿using NUnit.Framework;
+using Zyl.VectorTraits;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Security.Cryptography;
+using System.IO;
+
+namespace Zyl.VectorTraits.Tests {
+    [TestFixture()]
+    public class MathTests {
+
+        [Test()]
+        public void FloorCompareRoundToEvenTest() {
+            TextWriter writer = Console.Out;
+            bool hideEquals = true;
+            const long countShowMax = 100;
+            const int stepCount = 2;
+            const double stepDelta = 1.0 / stepCount;
+            double rangeItemNumber = Math.Pow(2, 4);
+            long rangeItemCount = (long)(rangeItemNumber * stepCount);
+            double[] rangeStarts = new double[] {
+                    -Math.Pow(2, 51),
+                    -Math.Pow(2, 50),
+                    0,
+                    Math.Pow(2, 50),
+                    Math.Pow(2, 51) - rangeItemNumber,
+                    // Out of range 52.
+                    Math.Pow(2, 51),
+                    Math.Pow(2, 52) - rangeItemNumber,
+                    Math.Pow(2, 52),
+            };
+            long countError = 0;
+            long countShow = 0;
+            if (rangeStarts.Length > 0) {
+                foreach (double start in rangeStarts) {
+                    for (long i = 0; i < rangeItemCount; i++) {
+                        double number = start + i * stepDelta;
+                        double expected = Math.Floor(number);
+                        double dst = Math.Round(number - 0.5, MidpointRounding.ToEven);
+                        string msg = "";
+                        if (expected != dst) {
+                            ++countError;
+                            msg = string.Format("[{0}] {1}: {2}!={3} // 0x{4:X}", i, number, expected, dst, BitConverter.DoubleToInt64Bits(number));
+                        } else {
+                            if (!hideEquals) {
+                                msg = string.Format("[{0}] {1}: {2}=={3} // 0x{4:X}", i, number, expected, dst, BitConverter.DoubleToInt64Bits(number));
+                            }
+                        }
+                        if (!string.IsNullOrEmpty(msg)) {
+                            ++countShow;
+                            if (countShow <= countShowMax) {
+                                writer.WriteLine(msg);
+                            }
+                        }
+                    }
+                }
+            }
+            writer.WriteLine("countError: {0}", countError);
+            //Assert.Zero(countError);
+        }
+
+    }
+}
