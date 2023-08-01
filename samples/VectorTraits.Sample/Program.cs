53a54b94a6ec26711a1e291e2795346212209fe0
zhouyuelin <zyl910@sina.com>
2023-08-01 18:29:00 +0800

    Zyl.VectorTraits.Sample: Update sample code.

--- samples/VectorTraits.Sample/Program.cs
+++ samples/VectorTraits.Sample/Program.cs
@@ -1,83 +1,49 @@
 ﻿using System;
 using System.IO;
 using System.Numerics;
-using Zyl.VectorTraits;
-using Zyl.VectorTraits.Extensions.SameW;
-using Zyl.VectorTraits.Impl;
-using Zyl.VectorTraits.Impl.AVector;
 
 namespace Zyl.VectorTraits.Sample {
     class Program {
-        static TextWriter writer = Console.Out;
+        private static readonly TextWriter writer = Console.Out;
         static void Main(string[] args) {
             writer.WriteLine("VectorTraits.Sample");
             writer.WriteLine();
-            TraitsOutput.OutputEnvironment(writer);
+            VectorTraitsGlobal.Init(); // Initialization (初始化).
+            TraitsOutput.OutputEnvironment(writer); // Output environment info (输出环境信息).
+            writer.WriteLine(string.Format("Vectors.Instance:\t{0}", Vectors.Instance.GetType().Name));
             writer.WriteLine();
 
-            VectorTraitsGlobal.Init();
-            Vector<short> src = Vectors.CreateRotate<short>(0, 1, 2, 3, 4, 5, 6, 7);
-            VectorTextUtil.WriteLine(writer, "src:\t{0}", src);
-            // Truncate.
+            // -- Start --
+            Vector<short> src = Vectors.CreateRotate<short>(0, 1, 2, 3, 4, 5, 6, 7); // The `Vectors` class provides some methods. For example, 'CreateRotate' is rotate fill (`Vectors` 类提供了许多方法. 例如 `CreateRotate` 是旋转填充).
+            VectorTextUtil.WriteLine(writer, "src:\t{0}", src); // It can not only format the string, but also display the hexadecimal of each element in the vector on the right Easy to view vector data (它不仅能格式化字符串, 且会在右侧显示向量中各元素的十六进制. 便于查看向量数据).
+
+            // ShiftLeft. It is a new vector method in `.NET 7.0` (左移位. 它是 `.NET 7.0` 新增的向量方法)
+            Vector<short> shifted = Vectors.ShiftLeft(src, 1); // shifted[i] = src[i] << 1.
+            VectorTextUtil.WriteLine(writer, "ShiftLeft:\t{0}", shifted);
+#if NET7_0_OR_GREATER
+            VectorTextUtil.WriteLine(writer, "Equals to BCL ShiftLeft:\t{0}", shifted.Equals(Vector.ShiftLeft(src, 1))); // Compare the results of the same function in BCL with it (将BCL里相同函数的结果与它做对比).
+#endif
+            VectorTextUtil.WriteLine(writer, "Equals to ShiftLeft_Const:\t{0}", shifted.Equals(Vectors.ShiftLeft_Const(src, 1))); // If the parameter shiftAmount is a constant, you can also use the ShiftLeft of Vectors_ Const method It is faster in many scenarios (若参数 shiftAmount 是常数, 还可以使用 Vectors 的 ShiftLeft_Const 方法. 它在不少场景下更快).
             writer.WriteLine();
-            Vector<double>[] samples = {
-                Vectors<double>.Serial,
-                Vectors.CreateByDoubleLoop<double>(-0.5, 1),
-                Vectors.CreateByDoubleLoop<double>(-0.5, -1),
-                Vectors<double>.Demo,
-                Vectors<double>.DemoNaN,
-                Vectors.CreateByDoubleLoop<double>(BitConverter.Int64BitsToDouble(((ScalarConstants.Double_ExponentBias + 0L)<<52) | ScalarConstants.Double_MantissaMask), 1),
-                Vectors.CreateByDoubleLoop<double>(BitConverter.Int64BitsToDouble(((ScalarConstants.Double_ExponentBias + 1L)<<52) | ScalarConstants.Double_MantissaMask), 1),
-                Vectors.CreateByDoubleLoop<double>(BitConverter.Int64BitsToDouble(((ScalarConstants.Double_ExponentBias + 2L)<<52) | ScalarConstants.Double_MantissaMask), 1),
-                Vectors.CreateByDoubleLoop<double>(BitConverter.Int64BitsToDouble(((ScalarConstants.Double_ExponentBias + 50L)<<52) | ScalarConstants.Double_MantissaMask), 1),
-                Vectors.CreateByDoubleLoop<double>(BitConverter.Int64BitsToDouble(((ScalarConstants.Double_ExponentBias + 51L)<<52) | ScalarConstants.Double_MantissaMask), 1),
-                Vectors.CreateByDoubleLoop<double>(BitConverter.Int64BitsToDouble(((ScalarConstants.Double_ExponentBias + 52L)<<52) | ScalarConstants.Double_MantissaMask), 1),
-                Vectors.CreateByDoubleLoop<double>(BitConverter.Int64BitsToDouble(-((ScalarConstants.Double_ExponentBias + 50L)<<52) | ScalarConstants.Double_MantissaMask), 1),
-                Vectors.CreateByDoubleLoop<double>(BitConverter.Int64BitsToDouble(-((ScalarConstants.Double_ExponentBias + 51L)<<52) | ScalarConstants.Double_MantissaMask), 1),
-                Vectors.CreateByDoubleLoop<double>(BitConverter.Int64BitsToDouble(-((ScalarConstants.Double_ExponentBias + 52L)<<52) | ScalarConstants.Double_MantissaMask), 1),
-            };
-            foreach(var sample in samples) {
-                var expected = VectorTraitsBase.Statics.YRoundToZero_Floor(sample);
-                writer.WriteLine(VectorTextUtil.Format("Sample:\t{0}", sample));
-                writer.WriteLine(VectorTextUtil.Format("Expected:\t{0}", expected));
-                var dst = YRoundToZero_ClearBit(sample);
-                if (!dst.AsInt32().Equals(expected.AsInt32())) {
-                    writer.WriteLine(VectorTextUtil.Format("Dst:\t{0}", dst));
-                }
-                writer.WriteLine();
-            }
-        }
-        public static Vector<double> YRoundToZero_ClearBit(Vector<double> value) {
-            // Float encode is `sign*(1.m)*pow(2,e) = sign*(1.m)*pow(2,eBias - BIAS)`. Double's BIAS is `1023`.
-            // If (e>=52): Has 0bit fractional part. The mask is `0`.
-            // If (e==51): Has 1bit fractional part. The mask is `0x1`.
-            // ...
-            // If (e==1): Has 51bit fractional part. The mask is `0x0007FFFF_FFFFFFFF`.
-            // If (e==0): Has 52bit fractional part. The mask is `0x000FFFFF_FFFFFFFF`.
-            // If (e< 0): Need set to zero (0.0 / -0.0). The mask is `0x7FFFFFFF_FFFFFFFF`.
-            //
-            // If (0<=e && e<=52): The mask is `pow(2,52-e) - 1`. So `RoundToZero(x) = x & ~mask`.
-            //Constants.
-            Vector<double> exponentMask = new Vector<double>(ScalarConstants.DoubleVal_ExponentMask);
-            Vector<double> one = new Vector<double>(1.0);
-            Vector<double> rangeEnd = new Vector<double>(ScalarConstants.DoubleVal_2Pow52); // Double value: pow(2, 52)
-            Vector<double> nonSignMask = VectorConstants.Double_NonSignMask;
-            Vector<double> expMinuend = new Vector<double>(ScalarConstants.DoubleVal_Truncate_expMinuend); // Item is `(long)(1023*2 + 52)<<52`. Binary is `0x8320000000000000`.
-            //Operations.
-            Vector<double> valueExpData = Vector.BitwiseAnd(value, exponentMask); // Get exponent field.
-            Vector<long> maskBegin = Vector.GreaterThan(one, valueExpData); // `1 > valueExpData[i] = pow(2,0) > pow(2,e)`, it mean `e<0`.
-            valueExpData = Vector.Min(valueExpData, rangeEnd); // Clamp to `e<=52`.
-            maskBegin = Vector.BitwiseAnd(maskBegin, nonSignMask.AsInt64()); // Keep sign flag.
-            Vector<double> maskRawPow = Vector.Subtract(expMinuend.AsUInt64(), valueExpData.AsUInt64()).AsDouble(); // If valueExpData is `(1023 + e)<<52`, `expMinuend-valueExpData` exponent field will be `(1023*2 + 52) - (1023 + e) = 1023 + (52-e)`
-            maskRawPow = Vector.Subtract(maskRawPow, one); // The mask is `pow(2,52-e) - 1`.
-            Vector<long> mask = Vector.Add(maskRawPow, rangeEnd).AsInt64(); // Step 1 of ConvertToUInt64_Range52RoundToEven .
-            mask = Vector.Xor(mask, rangeEnd.AsInt64()); // mask = ConvertToUInt64_Range52RoundToEven(maskRawPow).AsInt64();
-            Vector<long> allBitsSet = Vectors<long>.AllBitsSet;
-            mask = Vector.BitwiseOr(mask, maskBegin); // Choose (e<0).
-            //writer.WriteLine(VectorTextUtil.Format("The mask:\t{0}", mask));
-            mask = Vector.Xor(mask, allBitsSet); // mask = ~mask;
-            Vector<double> rt = Vector.BitwiseAnd(value, mask.AsDouble());
-            return rt;
+
+            // Shuffle. It is a new vector method in `.NET 7.0` (换位. 它是 `.NET 7.0` 新增的向量方法)
+            Vector<short> desc = Vectors<short>.SerialDesc; // The generic structure 'Vectors<T>' provides fields for commonly used constants For example, 'SerialDesc' is a descending order value (泛型结构体 `Vectors<T>` 为常用常数提供了字段. 例如 `SerialDesc` 是降序的顺序值).
+            VectorTextUtil.WriteLine(writer, "desc:\t{0}", desc);
+            Vector<short> dst = Vectors.Shuffle(shifted, desc);
+            VectorTextUtil.WriteLine(writer, "Shuffle:\t{0}", dst);
+#if NET7_0_OR_GREATER
+            VectorTextUtil.WriteLine(writer, "Equals to BCL Shuffle:\t{0}", dst.Equals(Vectors.Shuffle(shifted, desc))); // Compare the results of the same function in BCL with it (将BCL里相同函数的结果与它做对比).
+#endif
+            // Shuffle_Args and Shuffle_Core
+            Vectors.Shuffle_Args(desc, out var args0, out var args1); // The suffix is the `Args' method used for parameter calculation, which involves processing such as parameter transformation in advance It is suitable for external loop (后缀是 `Args` 的方法, 用于参数计算, 即提前进行参数变换等处理. 它适合放在外循环).
+            Vector<short> dst2 = Vectors.Shuffle_Core(shifted, args0, args1); // The suffix is the `Core` method used for core calculations, which calculates based on cached parameters It is suitable for internal loop to improve performance (后缀是 `Core` 方法, 用于核心计算, 既根据已缓存的参数进行计算. 它适合放在内循环, 便于改善性能).
+            VectorTextUtil.WriteLine(writer, "Equals to Shuffle_Core:\t{0}", dst.Equals(dst2));
+            writer.WriteLine();
+
+            // Show AcceleratedTypes.
+            VectorTextUtil.WriteLine(writer, "ShiftLeft_AcceleratedTypes:\t{0}", Vectors.ShiftLeft_AcceleratedTypes);
+            VectorTextUtil.WriteLine(writer, "Shuffle_AcceleratedTypes:\t{0}", Vectors.Shuffle_AcceleratedTypes);
+
         }
     }
 }
