f7cec7a883d89cfb7d6482b4a09f3dfd6aebec91
zhouyuelin <zyl910@sina.com>
2023-08-02 15:36:59 +0800

    VectorTraits.Sample: Fix BCL Shuffle .

--- samples/VectorTraits.Sample/Program.cs
+++ samples/VectorTraits.Sample/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.IO;
 using System.Numerics;
+#if NETCOREAPP3_0_OR_GREATER
+using System.Runtime.Intrinsics;
+#endif
 
 namespace Zyl.VectorTraits.Sample {
     class Program {
@@ -21,18 +24,28 @@ namespace Zyl.VectorTraits.Sample {
             Vector<short> shifted = Vectors.ShiftLeft(src, 1); // shifted[i] = src[i] << 1.
             VectorTextUtil.WriteLine(writer, "ShiftLeft:\t{0}", shifted);
 #if NET7_0_OR_GREATER
-            VectorTextUtil.WriteLine(writer, "Equals to BCL ShiftLeft:\t{0}", shifted.Equals(Vector.ShiftLeft(src, 1))); // Compare the results of the same function in BCL with it (将BCL里相同函数的结果与它做对比).
+            // Compare BCL function (与BCL的函数做对比).
+            Vector<short> shiftedBCL = Vector.ShiftLeft(src, 1);
+            VectorTextUtil.WriteLine(writer, "Equals to BCL ShiftLeft:\t{0}", shifted.Equals(shiftedBCL));
 #endif
+            // ShiftLeft_Const
             VectorTextUtil.WriteLine(writer, "Equals to ShiftLeft_Const:\t{0}", shifted.Equals(Vectors.ShiftLeft_Const(src, 1))); // If the parameter shiftAmount is a constant, you can also use the ShiftLeft of Vectors_ Const method It is faster in many scenarios (若参数 shiftAmount 是常数, 还可以使用 Vectors 的 ShiftLeft_Const 方法. 它在不少场景下更快).
             writer.WriteLine();
 
             // Shuffle. It is a new vector method in `.NET 7.0` (换位. 它是 `.NET 7.0` 新增的向量方法)
             Vector<short> desc = Vectors<short>.SerialDesc; // The generic structure 'Vectors<T>' provides fields for commonly used constants For example, 'SerialDesc' is a descending order value (泛型结构体 `Vectors<T>` 为常用常数提供了字段. 例如 `SerialDesc` 是降序的顺序值).
             VectorTextUtil.WriteLine(writer, "desc:\t{0}", desc);
-            Vector<short> dst = Vectors.Shuffle(shifted, desc);
+            Vector<short> dst = Vectors.Shuffle(shifted, desc); // dst[i] = shifted[desc[i]].
             VectorTextUtil.WriteLine(writer, "Shuffle:\t{0}", dst);
 #if NET7_0_OR_GREATER
-            VectorTextUtil.WriteLine(writer, "Equals to BCL Shuffle:\t{0}", dst.Equals(Vectors.Shuffle(shifted, desc))); // Compare the results of the same function in BCL with it (将BCL里相同函数的结果与它做对比).
+            // Compare BCL function (与BCL的函数做对比). 
+            Vector<short> dstBCL = default; // Since `.NET 7.0`, the Shuffle method has been provided in Vector128/Vector256, but the Shuffle method has not yet been provided in Vector (自 `.NET 7.0` 开始, Vector128/Vector256 里提供了 Shuffle 方法, 但 Vector 里尚未提供 Shuffle 方法).
+            if (Vector<short>.Count == Vector128<short>.Count) {
+                dstBCL = Vector128.Shuffle(shifted.AsVector128(), desc.AsVector128()).AsVector();
+            } else if (Vector<short>.Count == Vector256<short>.Count) {
+                dstBCL = Vector256.Shuffle(shifted.AsVector256(), desc.AsVector256()).AsVector();
+            }
+            VectorTextUtil.WriteLine(writer, "Equals to BCL Shuffle:\t{0}", dst.Equals(dstBCL));
 #endif
             // Shuffle_Args and Shuffle_Core
             Vectors.Shuffle_Args(desc, out var args0, out var args1); // The suffix is the `Args' method used for parameter calculation, which involves processing such as parameter transformation in advance It is suitable for external loop (后缀是 `Args` 的方法, 用于参数计算, 即提前进行参数变换等处理. 它适合放在外循环).
