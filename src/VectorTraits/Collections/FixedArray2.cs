9f16b51bbcfe5a943523cf4c020e15150f64ca1c
zhouyuelin <zyl910@sina.com>
2023-10-09 22:28:30 +0800

    Add FixedArray2, IFixedArray. #4

--- src/VectorTraits/Collections/FixedArray2.cs
+++ src/VectorTraits/Collections/FixedArray2.cs
@@ -8,63 +8,36 @@ using System.Text;
 
 namespace Zyl.VectorTraits.Collections {
     /// <summary>
-    /// An array of fixed capacity 16 (容量固定为16的数组).
+    /// An array of fixed capacity 2 (容量固定为2的数组).
     /// </summary>
     /// <typeparam name="T">The array element type (数组中的元素的类型).</typeparam>
-    [DebuggerDisplay("Count={Count}, Items=[{I0}, {I1}, {I2}, {I3}, ...]")]
+    [DebuggerDisplay("Count={Count}, Items=[{I0}, {I1}]")]
 #if NET20_OR_GREATER || NETSTANDARD2_0_OR_GREATER || NETCOREAPP1_0_OR_GREATER
     [Serializable]
 #endif
     [StructLayout(LayoutKind.Sequential, Pack = 1)]
-    public struct FixedArray16<T> : IEquatable<FixedArray16<T>>, IFixedArray<T>, IReadOnlyList<T>
+    public struct FixedArray2<T> : IEquatable<FixedArray2<T>>, IFixedArray<T>, IReadOnlyList<T>
             where T : struct {
         /// <summary>Capacity value.</summary>
-        public const int CapacityValue = 16;
+        public const int CapacityValue = 2;
 
         // -- Items --
         /// <summary>Item 0.</summary>
         public T I0;
+        /// <summary>Item 1.</summary>
         public T I1;
-        public T I2;
-        public T I3;
-        public T I4;
-        public T I5;
-        public T I6;
-        public T I7;
-        public T I8;
-        public T I9;
-        public T I10;
-        public T I11;
-        public T I12;
-        public T I13;
-        public T I14;
-        public T I15;
 
         /// <inheritdoc/>
         public override readonly bool Equals(object? obj) {
             if (null == obj) return false;
-            if (!(obj is FixedArray16<T>)) return false;
-            return Equals((FixedArray16<T>)obj);
+            if (!(obj is FixedArray2<T>)) return false;
+            return Equals((FixedArray2<T>)obj);
         }
 
         /// <inheritdoc/>
-        public readonly bool Equals(FixedArray16<T> other) {
+        public readonly bool Equals(FixedArray2<T> other) {
             if (!I0.Equals(other.I0)) return false;
             if (!I1.Equals(other.I1)) return false;
-            if (!I2.Equals(other.I2)) return false;
-            if (!I3.Equals(other.I3)) return false;
-            if (!I4.Equals(other.I4)) return false;
-            if (!I5.Equals(other.I5)) return false;
-            if (!I6.Equals(other.I6)) return false;
-            if (!I7.Equals(other.I7)) return false;
-            if (!I8.Equals(other.I8)) return false;
-            if (!I9.Equals(other.I9)) return false;
-            if (!I10.Equals(other.I10)) return false;
-            if (!I11.Equals(other.I11)) return false;
-            if (!I12.Equals(other.I12)) return false;
-            if (!I13.Equals(other.I13)) return false;
-            if (!I14.Equals(other.I14)) return false;
-            if (!I15.Equals(other.I15)) return false;
             return true;
         }
 
@@ -72,20 +45,6 @@ namespace Zyl.VectorTraits.Collections {
         public override readonly int GetHashCode() {
             int rt = I0.GetHashCode();
             rt ^= I1.GetHashCode();
-            rt ^= I2.GetHashCode();
-            rt ^= I3.GetHashCode();
-            rt ^= I4.GetHashCode();
-            rt ^= I5.GetHashCode();
-            rt ^= I6.GetHashCode();
-            rt ^= I7.GetHashCode();
-            rt ^= I8.GetHashCode();
-            rt ^= I9.GetHashCode();
-            rt ^= I10.GetHashCode();
-            rt ^= I11.GetHashCode();
-            rt ^= I12.GetHashCode();
-            rt ^= I13.GetHashCode();
-            rt ^= I14.GetHashCode();
-            rt ^= I15.GetHashCode();
             return rt;
         }
 
@@ -122,12 +81,12 @@ namespace Zyl.VectorTraits.Collections {
         }
 
         /// <inheritdoc/>
-        public static bool operator ==(FixedArray16<T> left, FixedArray16<T> right) {
+        public static bool operator ==(FixedArray2<T> left, FixedArray2<T> right) {
             return left.Equals(right);
         }
 
         /// <inheritdoc/>
-        public static bool operator !=(FixedArray16<T> left, FixedArray16<T> right) {
+        public static bool operator !=(FixedArray2<T> left, FixedArray2<T> right) {
             return !(left == right);
         }
 
---
+++ src/VectorTraits/Collections/IFixedArray.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zyl.VectorTraits.Collections {
+    /// <summary>
+    /// An array of fixed capacity interface. It has no bounds checking to avoid performance loss. Span only supports stack memory, and it supports static memory (固定容量数组的接口. 它没有边界检查, 可避免性能损失. Span 仅支持栈内存, 而它支持静态内存).
+    /// </summary>
+    /// <typeparam name="T">The array element type (数组中的元素的类型).</typeparam>
+    public interface IFixedArray<T> : IEnumerable<T>, IEnumerable, IReadOnlyCollection<T> {
+        /// <summary>
+        /// Gets or sets the element at the specified index.
+        /// </summary>
+        /// <param name="index">The zero-based index of the element to get or set.</param>
+        /// <returns>The element at the specified index.</returns>
+        T this[int index] { get; set; }
+    }
+}
