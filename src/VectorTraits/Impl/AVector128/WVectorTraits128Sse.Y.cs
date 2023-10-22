62f7d168f7ed522f2af2a4203fd8e0853458252b
zhouyuelin <zyl910@sina.com>
2023-10-22 15:48:08 +0800

    WVectorTraits128Sse and more: Fixed YRoundToEven methods. #1

--- src/VectorTraits/Impl/AVector128/WVectorTraits128Sse.Y.cs
+++ src/VectorTraits/Impl/AVector128/WVectorTraits128Sse.Y.cs
@@ -238,13 +238,55 @@ namespace Zyl.VectorTraits.Impl.AVector128 {
             /// <inheritdoc cref="IWVectorTraits128.YRoundToEven(Vector128{float})"/>
             [MethodImpl(MethodImplOptions.AggressiveInlining)]
             public static Vector128<float> YRoundToEven(Vector128<float> value) {
-                return Sse41.RoundToNearestInteger(value);
+                if (Sse41.IsSupported) {
+                    return Sse41.RoundToNearestInteger(value);
+                } else {
+                    return YRoundToEven_Add(value);
+                }
             }
 
             /// <inheritdoc cref="IWVectorTraits128.YRoundToEven(Vector128{double})"/>
             [MethodImpl(MethodImplOptions.AggressiveInlining)]
             public static Vector128<double> YRoundToEven(Vector128<double> value) {
-                return Sse41.RoundToNearestInteger(value);
+                if (Sse41.IsSupported) {
+                    return Sse41.RoundToNearestInteger(value);
+                } else {
+                    return YRoundToEven_Add(value);
+                }
+            }
+
+            /// <inheritdoc cref="IWVectorTraits128.YRoundToEven(Vector128{float})"/>
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public static Vector128<float> YRoundToEven_Add(Vector128<float> value) {
+                // [Single type] If (0<=x && x<pow(2,23)), `round_to_even(x) = x + pow(2,23) - pow(2,23)`. Next generalize this approach to all number ranges.
+                Vector128<float> delta = Vector128.Create(ScalarConstants.SingleVal_2Pow23);
+                Vector128<float> signMask = Vector128Constants.Single_SignMask;
+                Vector128<float> valueAbs = Sse.AndNot(value, signMask);
+                Vector128<float> signData = Sse.And(value, signMask);
+                //Vector128<float> allowMask = LessThan(valueAbs, delta); // Allow is `(value[i] < pow(2,23) )`.
+                Vector128<float> allowMask = Sse.CompareGreaterThan(delta, valueAbs); // Allow is `(value[i] < pow(2,23) )`.
+                delta = Sse.Or(delta, signData);
+                delta = Sse.And(delta, allowMask);
+                Vector128<float> rt = Sse.Add(value, delta);
+                rt = Sse.Subtract(rt, delta);
+                return rt;
+            }
+
+            /// <inheritdoc cref="IWVectorTraits128.YRoundToEven(Vector128{double})"/>
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public static Vector128<double> YRoundToEven_Add(Vector128<double> value) {
+                // [Double type] If (0<=x && x<pow(2,52)), `round_to_even(x) = x + pow(2,52) - pow(2,52)`. Next generalize this approach to all number ranges.
+                Vector128<double> delta = Vector128.Create(ScalarConstants.DoubleVal_2Pow52);
+                Vector128<double> signMask = Vector128Constants.Double_SignMask;
+                Vector128<double> valueAbs = Sse2.AndNot(value, signMask);
+                Vector128<double> signData = Sse2.And(value, signMask);
+                //Vector128<double> allowMask = LessThan(valueAbs, delta); // Allow is `(value[i] < pow(2,52) )`.
+                Vector128<double> allowMask = Sse2.CompareGreaterThan(delta, valueAbs); // Allow is `(value[i] < pow(2,52) )`.
+                delta = Sse2.Or(delta, signData);
+                delta = Sse2.And(delta, allowMask);
+                Vector128<double> rt = Sse2.Add(value, delta);
+                rt = Sse2.Subtract(rt, delta);
+                return rt;
             }
 
 
