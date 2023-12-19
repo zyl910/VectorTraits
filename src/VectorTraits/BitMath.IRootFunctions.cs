87c2761b7ff44973692937d15de8fdc2d9805664
zyl910 <zyl910hero@gmail.com>
2023-12-19 14:14:23 +0800

    Move Sqrt and more methods from BitMath to MathIRootFunctions.

--- src/VectorTraits/BitMath.IRootFunctions.cs
+++ src/VectorTraits/BitMath.IRootFunctions.cs
@@ -3,86 +3,75 @@ using System.Collections.Generic;
 using System.Numerics;
 using System.Runtime.CompilerServices;
 using System.Text;
+using Zyl.VectorTraits.Numerics;
 
 namespace Zyl.VectorTraits {
     // IRootFunctions: https://learn.microsoft.com/en-us/dotnet/api/system.numerics.irootfunctions-1
     // Referenced from generic math (参考了泛型数学): https://learn.microsoft.com/en-us/dotnet/standard/generics/math
     partial class BitMath {
 
-        /// <inheritdoc cref="Sqrt(double)"/>
-        /// <seealso cref="MathF.Sqrt"/>
+        /// <inheritdoc cref="MathIRootFunctions.Sqrt(float)"/>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static float Sqrt(float x) {
-#if NETCOREAPP2_0_OR_GREATER || NETSTANDARD2_1_OR_GREATER
-            return MathF.Sqrt(x);
-#else
-            return (float)Math.Sqrt(x);
-#endif // NETCOREAPP2_0_OR_GREATER || NETSTANDARD2_1_OR_GREATER
+            return MathIRootFunctions.Sqrt(x);
         }
 
-        /// <summary>
-        /// Computes the square-root of a value (计算值的平方根).
-        /// Mnemonic: `Sqrt(x) = Pow(x, 1.0/2)`. When x is less than 0, floating-point types return NaN, integer types return 0.
-        /// </summary>
-        /// <param name="x">The value whose square-root is to be computed (要计算其平方根的值).</param>
-        /// <returns>The square-root of <paramref name="x"/> (<paramref name="x"/>的平方根).</returns>
-        /// <seealso cref="IRootFunctions{TSelf}.Sqrt(TSelf)"/>
-        /// <seealso cref="Math.Sqrt"/>
+        /// <inheritdoc cref="MathIRootFunctions.Sqrt(double)"/>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static double Sqrt(double x) {
-            return Math.Sqrt(x);
+            return MathIRootFunctions.Sqrt(x);
         }
 
-        /// <inheritdoc cref="Sqrt(double)"/>
+        /// <inheritdoc cref="MathIRootFunctions.Sqrt(sbyte)"/>
         [CLSCompliant(false)]
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static sbyte Sqrt(sbyte x) {
-            return ConditionalSelect(x < 0, (sbyte)0, (sbyte)Sqrt((byte)x));
+            return MathIRootFunctions.Sqrt(x);
         }
 
-        /// <inheritdoc cref="Sqrt(double)"/>
+        /// <inheritdoc cref="MathIRootFunctions.Sqrt(byte)"/>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static byte Sqrt(byte x) {
-            return (byte)Sqrt((float)x);
+            return MathIRootFunctions.Sqrt(x);
         }
 
-        /// <inheritdoc cref="Sqrt(double)"/>
+        /// <inheritdoc cref="MathIRootFunctions.Sqrt(short)"/>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static short Sqrt(short x) {
-            return ConditionalSelect(x < 0, (short)0, (short)Sqrt((ushort)x));
+            return MathIRootFunctions.Sqrt(x);
         }
 
-        /// <inheritdoc cref="Sqrt(double)"/>
+        /// <inheritdoc cref="MathIRootFunctions.Sqrt(ushort)"/>
         [CLSCompliant(false)]
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static ushort Sqrt(ushort x) {
-            return (ushort)Sqrt((float)x);
+            return MathIRootFunctions.Sqrt(x);
         }
 
-        /// <inheritdoc cref="Sqrt(double)"/>
+        /// <inheritdoc cref="MathIRootFunctions.Sqrt(int)"/>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static int Sqrt(int x) {
-            return ConditionalSelect(x < 0, (int)0, (int)Sqrt((uint)x));
+            return MathIRootFunctions.Sqrt(x);
         }
 
-        /// <inheritdoc cref="Sqrt(double)"/>
+        /// <inheritdoc cref="MathIRootFunctions.Sqrt(uint)"/>
         [CLSCompliant(false)]
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static uint Sqrt(uint x) {
-            return (uint)Sqrt((double)x);
+            return MathIRootFunctions.Sqrt(x);
         }
 
-        /// <inheritdoc cref="Sqrt(double)"/>
+        /// <inheritdoc cref="MathIRootFunctions.Sqrt(long)"/>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static long Sqrt(long x) {
-            return ConditionalSelect(x < 0, (long)0, (long)Sqrt((ulong)x));
+            return MathIRootFunctions.Sqrt(x);
         }
 
-        /// <inheritdoc cref="Sqrt(double)"/>
+        /// <inheritdoc cref="MathIRootFunctions.Sqrt(ulong)"/>
         [CLSCompliant(false)]
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static ulong Sqrt(ulong x) {
-            return (ulong)Sqrt((double)x);
+            return MathIRootFunctions.Sqrt(x);
         }
 
     }
