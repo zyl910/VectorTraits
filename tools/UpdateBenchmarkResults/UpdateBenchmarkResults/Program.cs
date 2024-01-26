7bfc7324d1e7d960401fd23f9dce0c8ca2d1abfa
zyl910 <zyl910hero@gmail.com>
2024-01-26 09:57:44 +0800

    Add tool program: UpdateBenchmarkResults.

---
+++ tools/UpdateBenchmarkResults/UpdateBenchmarkResults/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace UpdateBenchmarkResults {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
