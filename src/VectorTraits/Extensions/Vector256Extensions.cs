f4206eecf0f20f4a2456fab0a2c0dc9b01923d14
zyl910 <zyl910hero@gmail.com>
2024-05-21 23:46:11 +0800

    feat: Vector256Extensions add AsUnsigned methods. #12

--- src/VectorTraits/Extensions/Vector256Extensions.cs
+++ src/VectorTraits/Extensions/Vector256Extensions.cs
@@ -4,6 +4,11 @@
 #define ALLOW_EXTENSION
 #endif // NET5_0_OR_GREATER
 
+#if NETX_0_OR_GREATER
+#else
+#define ALLOW_EXTENSION_SIGNED
+#endif // NETX_0_OR_GREATER
+
 #endif // NETCOREAPP3_0_OR_GREATER
 
 using System;
@@ -44,6 +49,183 @@ namespace Zyl.VectorTraits.Extensions {
 #endif
                 Vector256<T> value) where T : struct {
             return Vectors.AsVector(value);
+        }
+
+
+        /// <summary>Reinterprets a unsigned integer vector as a new signed integer vector (将无符号整数向量, 重新解释为有符号整数向量).</summary>
+        /// <param name="value">The vector to reinterpret (将被重新解释的向量).</param>
+        /// <returns>Reinterpreted as a new vector (重新解释后的新向量).</returns>
+        [CLSCompliant(false)]
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static Vector256<sbyte> AsSigned(
+#if ALLOW_EXTENSION_SIGNED
+            this
+#endif
+                Vector256<sbyte> value) {
+            return value;
+        }
+
+        /// <inheritdoc cref="AsSigned(Vector256{sbyte})"/>
+        [CLSCompliant(false)]
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static Vector256<sbyte> AsSigned(
+#if ALLOW_EXTENSION_SIGNED
+            this
+#endif
+                Vector256<byte> value) {
+            return value.AsSByte();
+        }
+
+        /// <inheritdoc cref="AsSigned(Vector256{sbyte})"/>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static Vector256<short> AsSigned(
+#if ALLOW_EXTENSION_SIGNED
+            this
+#endif
+                Vector256<short> value) {
+            return value;
+        }
+
+        /// <inheritdoc cref="AsSigned(Vector256{sbyte})"/>
+        [CLSCompliant(false)]
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static Vector256<short> AsSigned(
+#if ALLOW_EXTENSION_SIGNED
+            this
+#endif
+                Vector256<ushort> value) {
+            return value.AsInt16();
+        }
+
+        /// <inheritdoc cref="AsSigned(Vector256{sbyte})"/>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static Vector256<int> AsSigned(
+#if ALLOW_EXTENSION_SIGNED
+            this
+#endif
+                Vector256<int> value) {
+            return value;
+        }
+
+        /// <inheritdoc cref="AsSigned(Vector256{sbyte})"/>
+        [CLSCompliant(false)]
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static Vector256<int> AsSigned(
+#if ALLOW_EXTENSION_SIGNED
+            this
+#endif
+                Vector256<uint> value) {
+            return value.AsInt32();
+        }
+
+        /// <inheritdoc cref="AsSigned(Vector256{sbyte})"/>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static Vector256<long> AsSigned(
+#if ALLOW_EXTENSION_SIGNED
+            this
+#endif
+                Vector256<long> value) {
+            return value;
+        }
+
+        /// <inheritdoc cref="AsSigned(Vector256{sbyte})"/>
+        [CLSCompliant(false)]
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static Vector256<long> AsSigned(
+#if ALLOW_EXTENSION_SIGNED
+            this
+#endif
+                Vector256<ulong> value) {
+            return value.AsInt64();
+        }
+
+        /// <summary>Reinterprets a signed integer vector as a new unsigned integer vector (将有符号整数向量, 重新解释为无符号整数向量).</summary>
+        /// <param name="value">The vector to reinterpret (将被重新解释的向量).</param>
+        /// <returns>Reinterpreted as a new vector (重新解释后的新向量).</returns>
+        [CLSCompliant(false)]
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static Vector256<byte> AsUnsigned(
+#if ALLOW_EXTENSION_SIGNED
+            this
+#endif
+                Vector256<sbyte> value) {
+            return value.AsByte();
+        }
+
+        /// <inheritdoc cref="AsUnsigned(Vector256{sbyte})"/>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static Vector256<byte> AsUnsigned(
+#if ALLOW_EXTENSION_SIGNED
+            this
+#endif
+                Vector256<byte> value) {
+            return value;
+        }
+
+        /// <inheritdoc cref="AsUnsigned(Vector256{sbyte})"/>
+        [CLSCompliant(false)]
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static Vector256<ushort> AsUnsigned(
+#if ALLOW_EXTENSION_SIGNED
+            this
+#endif
+                Vector256<short> value) {
+            return value.AsUInt16();
+        }
+
+        /// <inheritdoc cref="AsUnsigned(Vector256{sbyte})"/>
+        [CLSCompliant(false)]
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static Vector256<ushort> AsUnsigned(
+#if ALLOW_EXTENSION_SIGNED
+            this
+#endif
+                Vector256<ushort> value) {
+            return value;
+        }
+
+        /// <inheritdoc cref="AsUnsigned(Vector256{sbyte})"/>
+        [CLSCompliant(false)]
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static Vector256<uint> AsUnsigned(
+#if ALLOW_EXTENSION_SIGNED
+            this
+#endif
+                Vector256<int> value) {
+            return value.AsUInt32();
+        }
+
+        /// <inheritdoc cref="AsUnsigned(Vector256{sbyte})"/>
+        [CLSCompliant(false)]
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static Vector256<uint> AsUnsigned(
+#if ALLOW_EXTENSION_SIGNED
+            this
+#endif
+                Vector256<uint> value) {
+            return value;
+        }
+
+        /// <inheritdoc cref="AsUnsigned(Vector256{sbyte})"/>
+        [CLSCompliant(false)]
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static Vector256<ulong> AsUnsigned(
+#if ALLOW_EXTENSION_SIGNED
+            this
+#endif
+                Vector256<long> value) {
+            return value.AsUInt64();
+        }
+
+        /// <inheritdoc cref="AsUnsigned(Vector256{sbyte})"/>
+        [CLSCompliant(false)]
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static Vector256<ulong> AsUnsigned(
+#if ALLOW_EXTENSION_SIGNED
+            this
+#endif
+                Vector256<ulong> value) {
+            return value;
         }
 
 #endif // NETCOREAPP3_0_OR_GREATER
